0870e6790e29b5b40e3c4df6ab7d841857637683
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-03-21 22:41:58 +0100

    added webpack.devserver.config.js

---
+++ hrzoosignup/frontend/webpack.devserver.config.js
@@ -0,0 +1,64 @@
+var BundleTracker = require('webpack-bundle-tracker');
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: "./src/main.jsx",
+  mode: "development",
+  output: {
+    publicPath: 'http://0.0.0.0:3000/',
+  },
+  devServer: {
+    open: false,
+    port: 3000,
+    hot: false,
+    liveReload: false,
+    historyApiFallback: true,
+    allowedHosts: 'all',
+    headers: { 'Access-Control-Allow-Origin': '*' },
+  },
+  plugins: [
+    new BundleTracker({filename: './webpack-stats.json'}),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader','css-loader', 'postcss-loader']
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          {
+            loader: "sass-loader",
+            options: {
+              // Prefer `dart-sass`
+              implementation: require('sass'),
+            },
+          }
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|otf)$/i,
+        type: 'asset/resource'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  }
+};
