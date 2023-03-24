a742470fb78d3290bd8327fe217edd046cc7d6b7
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-03-24 12:33:56 +0100

    have white navbar white red logo

--- hrzoosignup/frontend/src/ui/Navigation.jsx
+++ hrzoosignup/frontend/src/ui/Navigation.jsx
@@ -1,26 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Navbar,
   NavbarBrand,
   Nav,
-  NavbarText
+  NavItem,
+  Badge,
+  Popover,
+  PopoverBody,
+  Button,
 } from 'reactstrap';
-import {Link} from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faLaptopCode,
+  faSignOutAlt
 } from '@fortawesome/free-solid-svg-icons';
+import '../css/nav.css';
 
 
 const Navigation = () => {
+
+  const [popoverOpen, setPopoverOpen] = useState(false)
+
   return (
-    <Navbar className="fs-4 bg-danger" expand="md">
-      <NavbarBrand tag={Link} to="/home">
-        <FontAwesomeIcon icon={faLaptopCode} style={{color: "#c00000"}} size="2x" />
+    <Navbar expand="md" id="hzsi-nav" className="border rounded d-flex justify-content-between pt-3 pb-3">
+      <NavbarBrand href="https://www.srce.unizg.hr/napredno-racunanje"
+        target="_blank" rel="noopener noreferrer" className="text-dark">
+        <FontAwesomeIcon className="ps-4" icon={faLaptopCode} style={{color: "#c00000"}} size="3x" />
       </NavbarBrand>
-      <NavbarText>
-        Zahtjev za korištenjem usluge Napredno računanje
-      </NavbarText>
+      <Nav navbar className="m-1">
+        <span className="pl-3 font-weight-bold text-center">
+          <h2>
+            Zahtjev za korištenjem usluge Napredno računanje
+          </h2>
+        </span>
+      </Nav>
+      <Nav navbar >
+        <NavItem className='m-2 text-dark'>
+          <>
+            Dobrodošli,
+            <br/>
+            <span onClick ={() => setPopoverOpen(!popoverOpen)} id="userPopover">
+              <Badge href="#" className="text-dark" color="light"
+                style={{fontSize: '100%', textDecoration: 'none'}}>
+                <strong>Username</strong>
+              </Badge>
+            </span>
+            <Popover placement="bottom" isOpen={popoverOpen}
+              target="userPopover" toggle={() => setPopoverOpen(!popoverOpen)}>
+              <PopoverBody>
+                User details
+              </PopoverBody>
+            </Popover>
+          </>
+        </NavItem>
+        <NavItem className='m-2 text-light'>
+          <Button
+            size="sm"
+            aria-label="Odjava"
+            className='btn-danger'
+            onClick={() => {}}>
+            <FontAwesomeIcon icon={faSignOutAlt} color="white" />
+          </Button>
+        </NavItem>
+      </Nav>
     </Navbar>
   );
 };
