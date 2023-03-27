35a0bc6fd88be5ab030851889208b067ef4993d0
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-03-28 01:26:20 +0200

    fix NavLink by using new component from react-router-dom

--- hrzoosignup/frontend/src/ui/NavigationLinks.jsx
+++ hrzoosignup/frontend/src/ui/NavigationLinks.jsx
@@ -1,52 +1,45 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
   Nav,
   NavItem,
-  NavLink,
 } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import '../css/nav.css';
 
 
 const NavigationLinks = () => {
+  const activeBgColor = '#b04c46';
+
   return (
     <Nav tabs id="hzsi-navlinks" className="border-start border-end rounded d-flex justify-content-center sticky-top">
       <NavItem key='moji-zahtjevi' className='mt-1'>
         <NavLink
-          tag={Link}
-          active={location.pathname.split('/')[2] === 'moji-zahtjevi' ? true : false}
-          className={location.pathname.split('/')[2] === 'moji-zahtjevi' ? "text-white" : "text-dark"}
-          id={location.pathname.split('/')[2] === 'moji-zahtjevi' ? "hzsi-bg-danger" : ""}
+          style={({isActive}) => isActive ? {'backgroundColor': activeBgColor} : {}}
+          className={({isActive}) => isActive ? "nav-link active text-white" : "nav-link text-dark"}
           to='/ui/moji-zahtjevi'>
           Moji zahtjevi
         </NavLink>
       </NavItem>
       <NavItem key='novi-zahtjev' className='mt-1'>
         <NavLink
-          tag={Link}
-          active={location.pathname.split('/')[2] === 'novi-zahtjev' ? true : false}
-          className={location.pathname.split('/')[2] === 'novi-zahtjev' ? "text-white hzsi-bg-danger" : "text-dark"}
-          id={location.pathname.split('/')[2] === 'novi-zahtjev' ? "hzsi-bg-danger" : ""}
+          style={({isActive}) => isActive ? {'backgroundColor': activeBgColor} : {}}
+          className={({isActive}) => isActive ? "nav-link active text-white" : "nav-link text-dark"}
           to='/ui/novi-zahtjev'>
           Novi zahtjev
         </NavLink>
       </NavItem>
       <NavItem key='iskoristenost-resursa' className='mt-1'>
         <NavLink
-          tag={Link}
-          active={location.pathname.split('/')[2] === 'iskoristenost-resursa' ? true : false}
-          className={location.pathname.split('/')[2] === 'iskoristenost-resursa' ? "text-white" : "text-dark"}
-          id={location.pathname.split('/')[2] === 'iskoristenost-resursa' ? "hzsi-bg-danger" : ""}
+          style={({isActive}) => isActive ? {'backgroundColor': activeBgColor} : {}}
+          className={({isActive}) => isActive ? "nav-link active text-white" : "nav-link text-dark"}
           to='/ui/iskoristenost-resursa'>
           Iskorištenost resursa
         </NavLink>
       </NavItem>
-      <NavItem key='javni-resursa' className='mt-1'>
+      <NavItem key='javni-kljucevi' className='mt-1'>
         <NavLink
-          tag={Link}
-          active={location.pathname.split('/')[2] === 'iskoristenost-resursa' ? true : false}
-          className={location.pathname.split('/')[2] === 'iskoristenost-resursa' ? "text-white" : "text-dark"}
-          id={location.pathname.split('/')[2] === 'iskoristenost-resursa' ? "hzsi-bg-danger" : ""}
+          style={({isActive}) => isActive ? {'backgroundColor': activeBgColor} : {}}
+          className={({isActive}) => isActive ? "nav-link active text-white" : "nav-link text-dark"}
           to='/ui/javni-kljucevi'>
           Javni ključevi
         </NavLink>
