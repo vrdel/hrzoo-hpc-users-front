1b90491a52a6bba37e90a6ea746e34fee8634585
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-03-28 00:02:06 +0200

    comment accessibility link for now

--- hrzoosignup/frontend/src/routes.jsx
+++ hrzoosignup/frontend/src/routes.jsx
@@ -6,6 +6,7 @@ import BasePage from './ui/BasePage';
 import LoginPrivate from './routes/login-private';
 import LoginPublic from './routes/login-public';
 import MyRequests from './routes/my-requests';
+import NewRequest from './routes/new-request';
 import NotFound from './routes/notfound';
 import Root from './routes/root';
 
@@ -17,8 +18,9 @@ const BaseRoutes = () => {
         <Route path="ui" element={<Root />}>
           <Route path="prijava-priv" element={<LoginPrivate />}/>
           <Route path="prijava" element={<LoginPublic />}/>
-          <Route element={<BasePage/>}>
+          <Route element={<BasePage />}>
             <Route path="moji-zahtjevi" element={<MyRequests />}/>
+            <Route path="novi-zahtjev" element={<NewRequest />}/>
           </Route>
           <Route path="*" element={<NotFound />}/>
         </Route>
--- hrzoosignup/frontend/src/ui/Footer.jsx
+++ hrzoosignup/frontend/src/ui/Footer.jsx
@@ -8,11 +8,13 @@ const Footer = () => {
 
   return (
     <div id="hzsi-footer" className="align-self-center border rounded pristupacnost">
-      <div className="text-center mt-2">
-        <Link to="/ui/izjava-pristupacnost">
-          Izjava o pristupačnosti
-        </Link>
-      </div>
+      {
+        //<div className="text-center mt-2">
+          //<Link to="/ui/izjava-pristupacnost">
+            //Izjava o pristupačnosti
+          //</Link>
+        //</div>
+      }
       <div className="text-center pt-1 pb-2">
         <a href="https://www.srce.unizg.hr/" target="_blank" rel="noopener noreferrer">
           <img src={SrceBigLogo} id="srcelogo" alt="SRCE Logo"/>
