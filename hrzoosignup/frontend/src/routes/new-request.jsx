294a450d82e7d4ac08cd450e7e8da210732e5901
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-03-28 01:26:55 +0200

    initial component NewRequest

---
+++ hrzoosignup/frontend/src/routes/new-request.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const NewRequest = () => {
+  console.log('VRDEL DEBUG', location)
+  return (
+    <div>
+      Novi zahtjev
+    </div>
+  )
+};
+
+export default NewRequest;
