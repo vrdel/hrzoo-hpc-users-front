4cd06cdec78d608364901d02464b0b140851a72b
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-03-29 16:01:20 +0200

    install react-date-picker and have initial render on general request page

--- hrzoosignup/frontend/src/pages/new-requests/general.jsx
+++ hrzoosignup/frontend/src/pages/new-requests/general.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import RequestHorizontalRuler from '../../components/RequestHorizontalRuler';
-import { Col, Row, Button, Label, Input } from 'reactstrap';
+import { Col, Row, Button, Label  } from 'reactstrap';
+import DatePicker from 'react-date-picker';
+import 'react-date-picker/dist/DatePicker.css';
+import 'react-calendar/dist/Calendar.css';
+
 
 const GeneralRequest = () => {
+  const [fromDate, setFromDate] = useState(undefined)
+  const [toDate, setToDate] = useState(undefined)
+
   return (
     <>
       <RequestHorizontalRuler />
@@ -31,24 +38,39 @@ const GeneralRequest = () => {
         <Col>
         </Col>
       </Row>
-      <Row>
+      <Row className="mt-3">
         <Col md={{size: 10, offset: 1}}>
+          <Label
+            htmlFor="requestExplain"
+            aria-label="requestExplain"
+            className="mr-2 text-right form-label">
+            Obrazloženje:
+          </Label>
+          <textarea
+            id="requestExplain"
+            aria-label="requestExplain"
+            type="text"
+            className="form-control"
+            rows="3"
+            required={true}
+          />
+        </Col>
+        <Col>
+        </Col>
+      </Row>
+      <Row className="mt-3">
+        <Col md={{size: 5, offset: 1}}>
           <Label
             htmlFor="requestName"
             aria-label="requestName"
             className="mr-2 text-right form-label">
             Period korištenja:
           </Label>
-          <textarea
-            id="requestName"
-            aria-label="requestName"
-            type="text"
-            className="form-control"
-            rows="1"
-            required={true}
-          />
         </Col>
-        <Col>
+        <Col md={{size: 10, offset: 1}} style={{whiteSpace: 'nowrap'}}>
+          <DatePicker className="mt-2 me-3" onChange={setFromDate} />
+          {'\u2212'}
+          <DatePicker className="ms-3" onChange={setFromDate} />
         </Col>
       </Row>
       <RequestHorizontalRuler />
