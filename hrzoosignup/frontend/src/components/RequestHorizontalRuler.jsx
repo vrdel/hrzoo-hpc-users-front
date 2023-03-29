226ecb750c8f34c25455779d0e5a58ae24b59339
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-03-29 13:51:14 +0200

    introduced hr components and have first fields on general request

---
+++ hrzoosignup/frontend/src/components/RequestHorizontalRuler.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const RequestHorizontalRuler = () => (
+  <div>
+    <hr className="m-4 mt-5 mb-5"/>
+  </div>
+)
+
+export default RequestHorizontalRuler
--- hrzoosignup/frontend/src/pages/new-requests/general.jsx
+++ hrzoosignup/frontend/src/pages/new-requests/general.jsx
@@ -1,11 +1,62 @@
 import React from 'react'
+import RequestHorizontalRuler from '../../components/RequestHorizontalRuler';
+import { Col, Row, Button, Label, Input } from 'reactstrap';
 
 const GeneralRequest = () => {
   return (
     <>
-      <div>
-        Opca komponenta
-      </div>
+      <RequestHorizontalRuler />
+      <Row>
+        <Col>
+          <h4 className="ms-4 mb-3 mt-4">Opći dio</h4><br/>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={{size: 10, offset: 1}}>
+          <Label
+            htmlFor="requestName"
+            aria-label="requestName"
+            className="mr-2 text-right form-label">
+              Naziv:
+          </Label>
+          <textarea
+            id="requestName"
+            aria-label="requestName"
+            type="text"
+            className="form-control"
+            rows="1"
+            required={true}
+          />
+        </Col>
+        <Col>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={{size: 10, offset: 1}}>
+          <Label
+            htmlFor="requestName"
+            aria-label="requestName"
+            className="mr-2 text-right form-label">
+            Period korištenja:
+          </Label>
+          <textarea
+            id="requestName"
+            aria-label="requestName"
+            type="text"
+            className="form-control"
+            rows="1"
+            required={true}
+          />
+        </Col>
+        <Col>
+        </Col>
+      </Row>
+      <RequestHorizontalRuler />
+      <Row className="mt-2 mb-2 text-center">
+        <Col>
+          <Button size="lg" color="success" id="submit-button" type="submit">Podnesi zahtjev</Button>
+        </Col>
+      </Row>
     </>
   )
 };
