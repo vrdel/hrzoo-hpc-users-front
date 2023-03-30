6f7ebde6bdb59affcca4e68efac126d1ac461ce2
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-03-30 10:23:05 +0200

    add predefined scientific domains

---
+++ hrzoosignup/frontend/src/utils/scientific-domain.jsx
@@ -0,0 +1,87 @@
+let naturalFields = [
+  {
+    "name": "Matematika"
+  },
+  {
+    "name": "Fizika"
+  },
+  {
+    "name": "Geologija"
+  },
+  {
+    "name": "Kemija"
+  },
+  {
+    "name": "Biologija"
+  },
+  {
+    "name": "Geofizika"
+  },
+  {
+    "name": "Interdisciplinarne prirodne znanosti"
+  }
+]
+let naturalDomain = {
+  "name": "PRIRODNE ZNANOSTI",
+  "fields": naturalFields
+}
+
+let techFields = [
+  {
+    "name": "Arhitektura i urbanizam"
+  },
+  {
+    "name": "Brodogradnja"
+  },
+  {
+    "name": "Elektrotehnika"
+  },
+  {
+    "name": "Geodezija"
+  },
+  {
+    "name": "Građevinarstvo"
+  },
+  {
+    "name": "Grafička tehnologija"
+  },
+  {
+    "name": "Kemijsko inženjerstvo"
+  },
+  {
+    "name": "Metalurgija"
+  },
+  {
+    "name": "Računarstvo"
+  },
+  {
+    "name": "Rudarstvo, nafta i geološko inženjerstvo"
+  },
+  {
+    "name": "Strojarstvo"
+  },
+  {
+    "name": "Tehnologija prometa i transporta"
+  },
+  {
+    "name": "Tekstilna tehnologija"
+  },
+  {
+    "name": "Zrakoplovstvo, raketna i svemirska tehnika"
+  },
+  {
+    "name": "Temeljne tehničke znanosti"
+  },
+  {
+    "name": "Interdisciplinarne tehničke znanosti"
+  },
+]
+
+let techDomain = {
+  "name": "TEHNICKE ZNANOSTI",
+  "fields": techFields
+}
+
+export const scientificDomains = {
+  naturalDomain, techDomain
+}
