9e55cf6e4a527e514a90a6836d9860c648ff0e37
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-04-03 22:27:01 +0200

    simplifed error thrown

--- hrzoosignup/frontend/src/api/croris.jsx
+++ hrzoosignup/frontend/src/api/croris.jsx
@@ -2,19 +2,13 @@ import { url_api_prefix } from '../config/general';
 
 export async function fetchCroRIS()
 {
-  let err_msg = ''
-
   try {
     let response = await fetch(`${url_api_prefix}/api/v1/internal/croris-info/`)
-    if (response.ok)
+    if (response.ok) {
       return response.json()
-    else
-      return false
+    }
   } catch (err) {
-    err_msg = `${err} in fetch ${url_api_prefix}/api/v1/internal/croris-info/`
+    throw new Error(err)
   }
-
-  if (err_msg)
-    throw Error(err_msg)
 }
 
