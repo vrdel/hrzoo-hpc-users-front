3e26681a5f639a1f7cba8daa2e13eeed067b227c
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-04-07 17:40:58 +0200

    remove memcache settings

--- hrzoosignup/hrzoosignup/settings-devserver.py
+++ hrzoosignup/hrzoosignup/settings-devserver.py
@@ -258,19 +258,6 @@ WEBPACK_LOADER = {
 }
 SECURE_SSL_REDIRECT = False
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
-        'LOCATION': '127.0.0.1:11211',
-        'OPTIONS': {
-            'no_delay': True,
-            'ignore_exc': True,
-            'max_pool_size': 4,
-            'use_pooling': True,
-        }
-    }
-}
-
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
