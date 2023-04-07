082ecf69eeb6c45700b1266d7f9079a34de73dad
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-04-07 17:40:30 +0200

    add DRF permission settings

--- hrzoosignup/hrzoosignup/settings-devserver.py
+++ hrzoosignup/hrzoosignup/settings-devserver.py
@@ -270,3 +270,14 @@ CACHES = {
         }
     }
 }
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
+
