1263e1cfe62905aea22cca396aca1d3037077b42
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-04-07 12:35:08 +0200

    custom SAML2 backend with hooks if we are going to need them

--- hrzoosignup/backend/auth/saml2/backends.py
+++ hrzoosignup/backend/auth/saml2/backends.py
@@ -3,12 +3,10 @@ from django.core.cache import cache
 from djangosaml2.backends import Saml2Backend
 from django.contrib.auth import get_user_model
 
+# SAML2 hooks - left defaults, no overrides for now
 
 class SAML2Backend(Saml2Backend):
     def _update_user(self, user, attributes, attribute_mapping, force_save=False):
-        oib = attributes['hrEduPersonOIB']
-        uid = attributes['hrEduPersonUniqueID']
-        cache.set(f'{uid}_oib', oib, 3600 * 5)
         return super()._update_user(user, attributes, attribute_mapping, force_save)
 
     def save_user(self, user, *args, **kwargs):
