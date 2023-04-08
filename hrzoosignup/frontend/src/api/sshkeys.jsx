7735bf4326e6d37d3fca59c8d63824b0a70e1899
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-04-08 11:49:41 +0200

    refined DELETE fetch API method so that it handles all errors

--- hrzoosignup/frontend/src/api/sshkeys.jsx
+++ hrzoosignup/frontend/src/api/sshkeys.jsx
@@ -4,7 +4,8 @@ import Cookies from 'universal-cookie';
 
 export async function deleteSshKey(name)
 {
-  let cookies = new Cookies();
+  let cookies = new Cookies()
+  let error_msg = ''
 
   try {
     let response = await fetch(`${url_api_prefix}/api/v1/internal/keys/`, {
@@ -22,21 +23,55 @@ export async function deleteSshKey(name)
         'name': name
       })
     })
+
     if (response.ok)
       return response.json()
-  } catch (err) {
-    throw new Error(`Error delete SSH Key: ${err.message}`)
+
+    else if (!response.ok) {
+      try {
+        await response.json();
+        error_msg = `${response.status} ${response.statusText} in DELETE`
+      }
+      catch (err1) {
+        error_msg = `${response.status} ${response.statusText} in DELETE`
+      }
+
+    }
   }
+  catch (err) {
+    error_msg = `${err} in DELETE`;
+  }
+
+  if (error_msg)
+    throw new Error(`Error delete SSH Key: ${error_msg}`)
 }
 
 
 export async function fetchSshKeys()
 {
+  let error_msg = ''
+
   try {
     let response = await fetch(`${url_api_prefix}/api/v1/internal/keys/`)
+
     if (response.ok)
       return response.json()
-  } catch (err) {
-    throw new Error(`Error fetching SSHKeys data: ${err.message}`)
+
+    else if (!response.ok) {
+      try {
+        await response.json();
+        error_msg = `${response.status} ${response.statusText} in GET`
+      }
+      catch (err1) {
+        error_msg = `${response.status} ${response.statusText} in GET`
+      }
+    }
+
   }
+  catch (err) {
+    error_msg = `${err} in GET`;
+  }
+
+  if (error_msg)
+    throw new Error(`Error fetching SSHKeys data: ${error_msg}`)
 }
