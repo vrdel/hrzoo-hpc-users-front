d143aa7a152683685ad410491c8df6876d1e39c4
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-04-08 11:35:27 +0200

    backend DELETE for ssh key

--- hrzoosignup/frontend/src/api/sshkeys.jsx
+++ hrzoosignup/frontend/src/api/sshkeys.jsx
@@ -1,4 +1,34 @@
 import { url_api_prefix } from '../config/general';
+import Cookies from 'universal-cookie';
+
+
+export async function deleteSshKey(name)
+{
+  let cookies = new Cookies();
+
+  try {
+    let response = await fetch(`${url_api_prefix}/api/v1/internal/keys/`, {
+      method: 'DELETE',
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'same-origin',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'X-CSRFToken': cookies.get('csrftoken'),
+        'Referer': 'same-origin'
+      },
+      body: name && JSON.stringify({
+        'name': name
+      })
+    })
+    if (response.ok)
+      return response.json()
+  } catch (err) {
+    throw new Error(`Error delete SSH Key: ${err.message}`)
+  }
+}
+
 
 export async function fetchSshKeys()
 {
