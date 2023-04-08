1c6083ee95d6b2563caee755688edbc28d99c1bd
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-04-08 14:59:10 +0200

    remove react-hook-from as we don't submit anything

--- hrzoosignup/frontend/src/pages/public-keys.jsx
+++ hrzoosignup/frontend/src/pages/public-keys.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useState, useEffect } from 'react';
 import { SharedData } from './root';
 import { Col, Row, Table, Form, Collapse, Button, InputGroup, Input, InputGroupText } from 'reactstrap';
 import { PageTitle } from '../components/PageTitle';
-import { useQuery, useMutation } from '@tanstack/react-query';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { fetchSshKeys, deleteSshKey } from '../api/sshkeys';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
@@ -10,10 +10,9 @@ import {
   faArrowDown,
   faTimes,
 } from '@fortawesome/free-solid-svg-icons';
-import { useForm, Controller, useWatch, useFieldArray } from "react-hook-form";
 import '../styles/content.css';
 import ModalAreYouSure from '../components/ModalAreYouSure';
-import { toast, ToastContainer } from 'react-toastify'
+import { toast } from 'react-toastify'
 
 
 
@@ -26,6 +25,9 @@ const PublicKeys = () => {
   const [modalMsg, setModalMsg] = useState(undefined)
   const [onYesCall, setOnYesCall] = useState(undefined)
   const [onYesCallArg, setOnYesCallArg] = useState(undefined)
+  const [sshKeys, setSshKeys] = useState(undefined)
+
+  const queryClient = useQueryClient();
 
   const showKey = (keyid) => {
     let showed = new Object()
@@ -53,7 +55,7 @@ const PublicKeys = () => {
   useEffect(() => {
     setPageTitle(LinkTitles(location.pathname))
     if (sshKeysData?.length > 0)
-      setValue('sshKeys', sshKeysData)
+      setSshKeys(sshKeysData)
   }, [location.pathname, sshKeysData])
 
 
@@ -63,21 +65,27 @@ const PublicKeys = () => {
     },
   })
   const doDelete = (keyname) => deleteMutation.mutate(keyname, {
-    onSuccess: (e) => {
+    onSuccess: () => {
+      queryClient.invalidateQueries('ssh-keys');
       toast.success(
-        <span className="font-monospace">
+        <span className="font-monospace text-dark">
           Javni ključ uspješno izbrisan
         </span>, {
           toastId: 'sshkey-ok-delete',
+          autoClose: 2500,
+          delay: 500
         }
       )
     },
-    onError: (e) => {
+    onError: (error) => {
       toast.error(
-        <span className="font-monospace">
-          Javni ključ nije bilo moguće izbrisati: { e.message }
+        <span className="font-monospace text-dark">
+          Javni ključ nije bilo moguće izbrisati:
+          { error.message }
         </span>, {
-          toastId: 'sshkey-fail-delete',
+          toastId: 'sshkey-ok-delete',
+          autoClose: 2500,
+          delay: 500
         }
       )
     }
@@ -89,22 +97,7 @@ const PublicKeys = () => {
     }
   }
 
-  const onSubmit = data => {
-    JSON.stringify(data, null, 2)
-  }
-
-  const { control, handleSubmit, setValue, formState: { errors } } = useForm({
-    defaultValues: {
-      sshKeys: sshKeysData
-    }
-  });
-
-  const { fields, remove } = useFieldArray({
-    control,
-    name: "sshKeys"
-  })
-
-  if (status === 'success' && sshKeysData.length > 0)
+  if (status === 'success' && sshKeys?.length > 0)
   {
     return (
       <>
@@ -119,84 +112,82 @@ const PublicKeys = () => {
           onYes={onYesCallback} />
         <Row className="mt-4 ms-4 me-4 mb-3">
           <Col>
-            <Form onSubmit={handleSubmit(() => {})} className="needs-validation">
-              <Table responsive hover>
-                <thead id="hzsi-thead" className="table-active align-middle text-center text-white">
-                  <tr className="border-bottom border-2 border-dark">
-                    <th className="fw-normal">
-                      Ime ključa
-                    </th>
-                    <th className="fw-normal">
-                      Digitalni otisak ključa
-                    </th>
-                    <th className="fw-normal">
-                      Tip
-                    </th>
-                    <th className="fw-normal">
-                      Akcije
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  { fields.map((key, index) =>
-                    <>
-                      <tr key={key.id}>
-                        <td className="p-3 align-middle text-center">
-                          { key.name }
-                        </td>
-                        <td className="p-3 align-middle text-center font-monospace" style={{maxLength: '5'}}>
-                          { key.fingerprint }
-                        </td>
-                        <td className="align-middle text-center">
-                          { key.public_key.split(' ')[0] }
-                        </td>
-                        <td className="align-middle text-center">
-                          <Button size="sm" color="primary" onClick={() => showKey(key.id)}>
-                            <FontAwesomeIcon icon={faArrowDown} />
-                          </Button>
-                          <Button size="sm" className="ms-2" color="danger" onClick={() => {
-                            setAreYouSureModal(!areYouSureModal)
-                            setModalTitle("Brisanje javnog ključa")
-                            setModalMsg("Da li ste sigurni da želite obrisati javni ključ?")
-                            setOnYesCall('doremovesshkey')
-                            setOnYesCallArg(key.name)
-                          }}>
-                            <FontAwesomeIcon icon={faTimes} />
-                          </Button>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td  className="p-0 m-0" colSpan="4">
-                          <Collapse className="m-2 p-2" isOpen={isShowed(key.id)}>
-                            <Row>
-                              <Col sm={{size: 11}}>
-                                <InputGroup>
-                                  <InputGroupText>
-                                    Javni ključ:
-                                  </InputGroupText>
-                                  <textarea
-                                    className="font-monospace form-control"
-                                    rows="5"
-                                    placeholder={
-                                      key.public_key
-                                    }
-                                  />
-                                </InputGroup>
-                              </Col>
-                              <Col>
-                                <Button size="sm" color="success">
-                                  <FontAwesomeIcon icon={faCopy} />
-                                </Button>
-                              </Col>
-                            </Row>
-                          </Collapse>
-                        </td>
-                      </tr>
-                    </>
-                  )}
-                </tbody>
-              </Table>
-            </Form>
+            <Table responsive hover>
+              <thead id="hzsi-thead" className="table-active align-middle text-center text-white">
+                <tr className="border-bottom border-2 border-dark">
+                  <th className="fw-normal">
+                    Ime ključa
+                  </th>
+                  <th className="fw-normal">
+                    Digitalni otisak ključa
+                  </th>
+                  <th className="fw-normal">
+                    Tip
+                  </th>
+                  <th className="fw-normal">
+                    Akcije
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                { sshKeys.map((key, index) =>
+                  <>
+                    <tr key={key.id}>
+                      <td className="p-3 align-middle text-center">
+                        { key.name }
+                      </td>
+                      <td className="p-3 align-middle text-center font-monospace" style={{maxLength: '5'}}>
+                        { key.fingerprint }
+                      </td>
+                      <td className="align-middle text-center">
+                        { key.public_key.split(' ')[0] }
+                      </td>
+                      <td className="align-middle text-center">
+                        <Button size="sm" color="primary" onClick={() => showKey(index)}>
+                          <FontAwesomeIcon icon={faArrowDown} />
+                        </Button>
+                        <Button size="sm" className="ms-2" color="danger" onClick={() => {
+                          setAreYouSureModal(!areYouSureModal)
+                          setModalTitle("Brisanje javnog ključa")
+                          setModalMsg("Da li ste sigurni da želite obrisati javni ključ?")
+                          setOnYesCall('doremovesshkey')
+                          setOnYesCallArg(key.name)
+                        }}>
+                          <FontAwesomeIcon icon={faTimes} />
+                        </Button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td  className="p-0 m-0" colSpan="4">
+                        <Collapse className="m-2 p-2" isOpen={isShowed(index)}>
+                          <Row>
+                            <Col sm={{size: 11}}>
+                              <InputGroup>
+                                <InputGroupText>
+                                  Javni ključ:
+                                </InputGroupText>
+                                <textarea
+                                  className="font-monospace form-control"
+                                  rows="5"
+                                  placeholder={
+                                    key.public_key
+                                  }
+                                />
+                              </InputGroup>
+                            </Col>
+                            <Col className="d-flex align-self-center align-content-center">
+                              <Button size="sm" className="ms-3" color="success">
+                                <FontAwesomeIcon icon={faCopy} />
+                              </Button>
+                            </Col>
+                          </Row>
+                        </Collapse>
+                      </td>
+                    </tr>
+                  </>
+                )}
+              </tbody>
+            </Table>
           </Col>
         </Row>
       </>
