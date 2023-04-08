86caf2453eb65c80a20aaf499621f2c58dd5280b
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-04-08 20:35:25 +0200

    initial public key add component

--- hrzoosignup/frontend/src/pages/public-keys-add.jsx
+++ hrzoosignup/frontend/src/pages/public-keys-add.jsx
@@ -4,11 +4,14 @@ import {
   Col,
   Row,
   Table,
-  Collapse,
   Button,
   InputGroup,
+  Input,
   InputGroupText,
-  Placeholder
+  Label,
+  Placeholder,
+  Form,
+  FormFeedback
 } from 'reactstrap';
 import { PageTitle } from '../components/PageTitle';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
@@ -18,17 +21,29 @@ import {
   faCopy,
   faArrowDown,
   faKey,
+  faPlus,
   faTimes,
 } from '@fortawesome/free-solid-svg-icons';
 import '../styles/content.css';
 import ModalAreYouSure from '../components/ModalAreYouSure';
-import { toast } from 'react-toastify';
-import { useNavigate } from 'react-router-dom';
+import { useForm, Controller } from 'react-hook-form';
+import { ErrorMessage } from '@hookform/error-message';
 
 
 const NewPublicKey = () => {
   const { LinkTitles } = useContext(SharedData);
   const [pageTitle, setPageTitle] = useState(undefined);
+
+  const { control, handleSubmit, formState: { errors } } = useForm({
+    defaultValues: {
+      key_name: '',
+      public_key: ''
+    }
+  });
+
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data, null, 2));
+  }
 
   useEffect(() => {
     setPageTitle(LinkTitles(location.pathname))
@@ -39,11 +54,78 @@ const NewPublicKey = () => {
       <Row>
         <PageTitle pageTitle={pageTitle}/>
       </Row>
-      <Row>
-        <Col>
-          foobar
-        </Col>
-      </Row>
+      <Form onSubmit={handleSubmit(onSubmit)} className="needs-validation">
+        <Row>
+          <Col className="mt-4" sm={{size: 3, offset: 1}}>
+            <Label for="key_name" className="fs-5 fw-bold">
+              Ime ključa:
+            </Label>
+            <InputGroup>
+              <Controller
+                name="key_name"
+                control={control}
+                rules={{required: true}}
+                render={ ({field}) =>
+                  <Input
+                    {...field}
+                    placeholder="moj-laptop"
+                    className={`form-control fs-5 ${errors && errors.key_name && "is-invalid"}`}
+                  />
+                }
+              />
+              <ErrorMessage
+                errors={errors}
+                name="key_name"
+                render={({ message }) =>
+                  <FormFeedback className="end-0">
+                    { message }
+                  </FormFeedback>
+                }
+              />
+            </InputGroup>
+          </Col>
+          <Col className="ms-4" sm={{size: 7}}>
+            <Label className="mt-4 fs-5 fw-bold" for="public_key">
+              Javni ključ:
+            </Label>
+            <InputGroup>
+              <Controller
+                name="public_key"
+                control={control}
+                rules={{required: true}}
+                render={ ({field}) =>
+                  <textarea
+                    name="public_key"
+                    {...field}
+                    aria-label="public_key"
+                    type="text"
+                    placeholder="ssh-rsa AAAAB3NzaC1yc2EAAAA... me@laptop"
+                    className={`font-monospace fs-5 form-control ${errors && errors.public_key ? "is-invalid" : ''}`}
+                    rows="12"
+                  />
+                }
+              />
+              <ErrorMessage
+                errors={errors}
+                name="public_key"
+                render={({ message }) =>
+                  <FormFeedback className="end-0">
+                    { message }
+                  </FormFeedback>
+                }
+              />
+            </InputGroup>
+          </Col>
+        </Row>
+        <Row className='mt-5 mb-5'>
+          <Col className="text-center">
+            <Button size="lg" className="mt-3" color="success" type="submit">
+              <FontAwesomeIcon icon={faPlus}/>{' '}
+              Dodaj
+            </Button>
+          </Col>
+        </Row>
+      </Form>
     </>
   )
 }
