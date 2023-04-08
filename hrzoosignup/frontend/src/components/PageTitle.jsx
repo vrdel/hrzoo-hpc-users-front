395139b8fc4f27311fdabd27cb97df45f61d0636
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-04-08 19:14:59 +0200

    add bit of margin on PageTitle

--- hrzoosignup/frontend/src/components/PageTitle.jsx
+++ hrzoosignup/frontend/src/components/PageTitle.jsx
@@ -4,7 +4,7 @@ import { Col } from 'reactstrap';
 export const PageTitle = ({pageTitle}) => {
   return (
     <Col className="ms-3 me-3 mt-2 p-2 mb-2 rounded bg-light">
-      <h4>
+      <h4 className="mt-2">
         { pageTitle }
       </h4>
     </Col>
