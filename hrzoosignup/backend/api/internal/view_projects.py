0f0f169d7dc1bf3d212237a932379d7db9fcf24b
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-04-09 15:11:43 +0200

    initial ProjectSerializer

--- hrzoosignup/backend/api/internal/view_projects.py
+++ hrzoosignup/backend/api/internal/view_projects.py
@@ -10,6 +10,8 @@ from rest_framework.permissions import AllowAny
 from django.conf import settings
 from django.core.cache import cache
 
+from backend.serializers import ProjectSerializer
+
 import json
 import datetime
 
@@ -19,12 +21,32 @@ class ProjectsResearch(APIView):
     permission_classes = (IsAuthenticated,)
 
     def __init__(self):
-
         pass
 
     def post(self, request):
         oib = request.user.person_oib
+        # TODO: validate data picked up from frontend with the CroRIS cached
+        # data
         croris_data = cache.get('{oib}_croris')
+
+        request.data['is_active'] = True
+        request.data['users'] = request.user.pk
+        serializer = ProjectSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            from pudb.remote import set_trace; set_trace(host='0.0.0.0')
+
+            err_status = status.HTTP_400_BAD_REQUEST
+            err_response = {
+                'status': {
+                    'code': err_status,
+                    'message': json.dumps(serializer.errors)
+                }
+            }
+            return Response(err_response, status=err_status)
 
 
 class Projects(APIView):
