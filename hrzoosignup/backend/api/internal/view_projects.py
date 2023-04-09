9f7de2d92e5f1ea277ac7b8ba4d7a545fecf8785
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-04-09 10:51:47 +0200

    initial extra views for handling POST of general and research project

--- hrzoosignup/backend/api/internal/view_projects.py
+++ hrzoosignup/backend/api/internal/view_projects.py
@@ -4,13 +4,33 @@ from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 
+# TODO: dev only
+from rest_framework.permissions import AllowAny
+
 from django.conf import settings
+from django.core.cache import cache
 
 import json
 import datetime
 
 
+class ProjectsResearch(APIView):
+    authentication_classes = (SessionAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def __init__(self):
+
+        pass
+
+    def post(self, request):
+        oib = request.user.person_oib
+        croris_data = cache.get('{oib}_croris')
+
+
 class Projects(APIView):
+    # authentication_classes = (SessionAuthentication,)
+    permission_classes = (AllowAny,)
+
     def __init__(self):
         pass
 
--- hrzoosignup/backend/api/internal/views.py
+++ hrzoosignup/backend/api/internal/views.py
@@ -1,3 +1,3 @@
 from .view_croris import CroRISInfo
 from .view_sshkeys import SshKeys
-from .view_projects import Projects
+from .view_projects import ProjectsResearch, Projects
