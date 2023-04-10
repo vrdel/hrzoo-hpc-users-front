8cf95de6666b27141d4a0803aa2ced295ce6eea4
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-04-10 22:25:00 +0200

    style NotFound a bit with SRCE broken logo

--- hrzoosignup/frontend/src/pages/notfound.jsx
+++ hrzoosignup/frontend/src/pages/notfound.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import {
+  Row,
+  Col,
+  Container,
+  Card,
+  CardBody,
+  CardHeader,
+  CardFooter
+} from 'reactstrap'
+import SrceBroken from '../assets/srce-broken.png';
 
 
 const NotFound = () => {
   return (
-    <div>
-      Nema ovdje nista!
-    </div>
+    <Container fluid className="pt-1 d-flex align-items-center justify-content-center" style={{minHeight: '100vh'}}>
+      <Row>
+        <Col>
+          <Card className="shadow-lg" style={{minHeight: '50vh'}}>
+            <CardHeader className="fs-2 bg-danger fw-bold text-center text-white">
+              Griješka! SRCE mi se slama!
+            </CardHeader>
+            <CardBody className="p-5 mb-3">
+              <img src={SrceBroken} id="srcebrokenlogo" alt="SRCE Broken Logo"/>
+            </CardBody>
+            <CardFooter className="bg-danger text-white fw-bold text-center fs-4">
+              Nemoj više!
+            </CardFooter>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   )
 };
 
