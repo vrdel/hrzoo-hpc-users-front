a9c1795f87647f9185b6407beea5efb27d5c093d
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-04-14 00:56:38 +0200

    add rest of icons for complete set of states we're dealing with

--- hrzoosignup/frontend/src/config/map-states.jsx
+++ hrzoosignup/frontend/src/config/map-states.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  faArrowRight,
   faCheckDouble,
   faCog,
   faTimes,
+  faTimeline,
+  faCalendarXmark,
 } from '@fortawesome/free-solid-svg-icons';
 
 
@@ -12,7 +13,9 @@ export function StateIcons(state_name) {
   let state2icon = {
     'submit': <FontAwesomeIcon className="text-warning fa-3x" icon={faCog}/>,
     'approve': <FontAwesomeIcon className="text-success fa-3x" icon={faCheckDouble}/>,
-    'deny': <FontAwesomeIcon className="text-success fa-3x" icon={faTimes}/>
+    'deny': <FontAwesomeIcon className="text-danger fa-3x" icon={faTimes}/>,
+    'expire': <FontAwesomeIcon className="text-danger fa-3x" icon={faCalendarXmark}/>,
+    'extend': <FontAwesomeIcon className="text-warning fa-3x" icon={faTimeline}/>
   }
 
   return state2icon[state_name]
@@ -22,7 +25,9 @@ export function StateString(state_name) {
   let state2string = {
     'submit': 'Zahtjev je podnesen i čeka na obradu',
     'approve': 'Zahtjev je odobren',
-    'deny': 'Zahtjev je odbijen'
+    'deny': 'Zahtjev je odbijen',
+    'expire': 'Zahtjev je istekao',
+    'extend': 'Zahtjev čeka na produljenje'
   }
 
   return state2string[state_name]
