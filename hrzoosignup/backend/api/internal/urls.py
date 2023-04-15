dd9ea116b15f967947a960d5da0db068235c30ef
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-04-15 14:23:10 +0200

    signup redirect view with return back to authn context and user associating to project

--- hrzoosignup/backend/api/internal/urls.py
+++ hrzoosignup/backend/api/internal/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('projects-research/', views.ProjectsResearch.as_view(), name='projectsresearch'),
     path('projects-general/', views.ProjectsGeneral.as_view(), name='projectsgeneral'),
     path('invites/', views.Invites.as_view(), name='invites'),
+    path('invites-userlink/', views.InvitesLink.as_view(), name='inviteslink'),
     path('invites/<str:invitekey>', views.Invites.as_view(), name='invites'),
     path('keys/', views.SshKeys.as_view(), name='sshkeys'),
 ] + router.urls
--- hrzoosignup/backend/api/internal/view_invite.py
+++ hrzoosignup/backend/api/internal/view_invite.py
@@ -4,15 +4,35 @@ from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.permissions import AllowAny
 from rest_framework import status
-
 from rest_framework.renderers import JSONRenderer
 
 from invitations.utils import get_invitation_model
 
-from backend.models import Project
+from backend import models
 
 import json
 import requests
+import datetime
+
+
+def associate_user_to_project(user, project):
+    role_obj = models.Role.objects.get(name='collaborator')
+    userproject_obj = models.UserProject(user=user, project=project,
+                                         role=role_obj,
+                                         date_joined=datetime.datetime.now())
+    userproject_obj.save()
+
+
+# INVITATIONS_SIGNUP_REDIRECT setting
+# just return HTTP 200 OK here and that will return as back
+# in still authenticated context.
+class InvitesLink(APIView):
+    authentication_classes = (SessionAuthentication,)
+    permission_classes = (AllowAny,)
+
+    def get(self, request, **kwargs):
+        user = request.user.pk
+        return Response('ok, get us back', status.HTTP_200_OK)
 
 
 class Invites(APIView):
@@ -21,7 +41,7 @@ class Invites(APIView):
     permission_classes = (IsAuthenticated,)
 
     def get(self, request, **kwargs):
-        user = request.user.pk
+        user = request.user
 
         # hardcoding it here as
         # INVITATIONS_CONFIRMATION_URL_NAME did not help
@@ -33,7 +53,34 @@ class Invites(APIView):
         ))
         try:
             res = ret.raise_for_status()
+
+            if ret.status_code == 200 and 'invites-userlink' in ret.url:
+                Invitation = get_invitation_model()
+                get_invite = Invitation.objects.get(key=kwargs['invitekey'])
+                proj = get_invite.project
+                associate_user_to_project(user, proj)
+
+                return Response({
+                    'status': {
+                        'code': status.HTTP_201_CREATED,
+                        'message': '{} associated to project {}'.format(
+                            user.person_uniqueid,
+                            proj.identifier)
+                    }},
+                    status=status.HTTP_200_OK
+                )
+
         except requests.exceptions.HTTPError as ex:
+            if (ex.response.status_code == 403
+                and 'invites-userlink' in ex.response.url):
+                return Response({
+                    'status': {
+                        'code': status.HTTP_201_CREATED,
+                        'message': 'Invitation code used, user associated to invited project'
+                    }},
+                    status=status.HTTP_200_OK
+                )
+
             if ex.response.status_code == 410:
                 return Response({
                     'status': {
@@ -50,7 +97,7 @@ class Invites(APIView):
 
         Invitation = get_invitation_model()
 
-        proj = Project.objects.get(identifier='P-TIA-PERF-HZ2')
+        proj = models.Project.objects.get(identifier='NR-2023-04-001')
 
         invite = Invitation.create('daniel.vrcic@gmail.com', inviter=request.user, project=proj)
         invite.send_invitation(request)
