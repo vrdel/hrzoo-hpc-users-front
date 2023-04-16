c4f53698317574e7c922d7faa7c62d8b770362f4
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-04-16 14:54:17 +0200

    brief project info

--- hrzoosignup/frontend/src/pages/memberships.jsx
+++ hrzoosignup/frontend/src/pages/memberships.jsx
@@ -1,11 +1,59 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { SharedData } from './root';
-import { Col, Row, Card, CardHeader, CardBody } from 'reactstrap';
+import { Col, Row, Card, CardHeader, CardBody, Label, Badge } from 'reactstrap';
 import { PageTitle } from '../components/PageTitle';
 import { useQuery } from '@tanstack/react-query';
 import { fetchNrProjects } from '../api/projects';
 import { TypeString, TypeColor } from '../config/map-projecttypes';
+import { GeneralInfo, Persons, Finance, Summary } from '../components/GeneralProjectInfo';
+import { convertToEuropean, convertTimeToEuropean } from '../utils/dates';
 
+
+const BriefProjectInfo = ({project}) => {
+  return (
+    <>
+      <Col className="text-left" md={{size: 2}}>
+        Šifra:
+      </Col>
+      <Col md={{size: 3}}>
+        <Label
+          htmlFor="projectTime"
+          aria-label="projectTime"
+          className="mr-1">
+          Trajanje:
+        </Label>
+      </Col>
+      <Col md={{size: 4}}>
+        <Label
+          htmlFor="projectInstitution"
+          aria-label="projectInstitution"
+          className="mr-1">
+          Ustanova:
+        </Label>
+      </Col>
+
+      <div className="w-100"></div>
+
+      <Col md={{size: 2}}>
+        <div className="p-2">
+          <Badge color={"secondary fw-normal"}>
+            { project.identifier }
+          </Badge>
+        </div>
+      </Col>
+      <Col md={{size: 3}}>
+        <div className="p-2 fs-5 font-monospace">
+          { convertToEuropean(project.date_start) } &minus; { convertToEuropean(project.date_end) }
+        </div>
+      </Col>
+      <Col md={{size: 4}}>
+        <div className="p-2">
+          {project?.institute}<br/>
+        </div>
+      </Col>
+    </>
+  )
+}
 
 
 const Memberships = () => {
@@ -36,16 +84,19 @@ const Memberships = () => {
             projectsApproved.map((project, i) =>
               <Row className="mb-5" key={`row-${i}`}>
                 <Col key={`col-${i}`}>
-                  <Card className="ms-3" key={`card-${i}`}>
-                    <CardHeader className="d-flex justify-content-between">
-                      <span className="fs-5 fw-bold text-dark">
+                  <Card className="ms-3 border-0 bg-light shadow-sm" key={`card-${i}`}>
+                    <CardHeader className="d-flex border-0 justify-content-between">
+                      <span className="fs-5 text-dark fw-bold">
                         { project?.name }
                       </span>
                       <span className={`badge fw-normal ${TypeColor(project.project_type.name)}`} >
                         { TypeString(project.project_type.name) }
                       </span>
                     </CardHeader>
-                    <CardBody className="mb-1 bg-light">
+                    <CardBody className="border-0 bg-light">
+                      <Row>
+                        <BriefProjectInfo project={project} />
+                      </Row>
                     </CardBody>
                   </Card>
                 </Col>
