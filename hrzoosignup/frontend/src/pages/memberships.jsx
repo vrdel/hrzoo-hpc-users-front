b06461e4738946f7bd91e91d3f2fe6707602dac4
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-04-16 16:14:03 +0200

    add institute field on serializers

--- hrzoosignup/frontend/src/pages/memberships.jsx
+++ hrzoosignup/frontend/src/pages/memberships.jsx
@@ -77,7 +77,7 @@ const BriefProjectInfo = ({project}) => {
       </Col>
       <Col md={{size: 4}}>
         <div className="p-2">
-          {project?.institute}<br/>
+          {project.institute}<br/>
         </div>
       </Col>
     </>
@@ -137,7 +137,7 @@ const Memberships = () => {
           :
             <Row className="mt-3 mb-3">
               <Col className="d-flex align-items-center justify-content-center shadow-sm bg-light border border-danger rounded text-muted text-center p-3 fs-3" style={{height: '400px'}} md={{offset: 1, size: 10}}>
-                Nemate prijavljenih sudjelovanja na projektima
+                Nemate prijavljenih sudjelovanja na odobrenim projektima
               </Col>
             </Row>
         }
