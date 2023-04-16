858e3ae58e5aadec3940ed87cdc80d4ae8633b11
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-04-16 12:05:41 +0200

    empty view on memberships

--- hrzoosignup/frontend/src/pages/memberships.jsx
+++ hrzoosignup/frontend/src/pages/memberships.jsx
@@ -2,27 +2,45 @@ import React, { useContext, useState, useEffect } from 'react';
 import { SharedData } from './root';
 import { Col, Row } from 'reactstrap';
 import { PageTitle } from '../components/PageTitle';
+import { useQuery } from '@tanstack/react-query';
+import { fetchNrProjects } from '../api/projects';
+
 
 
 const Memberships = () => {
   const { LinkTitles } = useContext(SharedData);
   const [pageTitle, setPageTitle] = useState(undefined);
 
+  const {status: nrStatus, data: nrProjects} = useQuery({
+      queryKey: ['projects'],
+      queryFn: fetchNrProjects
+  })
+
   useEffect(() => {
     setPageTitle(LinkTitles(location.pathname))
   }, [location.pathname])
 
-  return (
-    <>
-      <Row>
-        <PageTitle pageTitle={pageTitle}/>
-      </Row>
-      <Row>
-        <Col>
-        </Col>
-      </Row>
-    </>
-  )
+  if (nrStatus === 'success' && nrProjects)
+    return (
+      <>
+        <Row>
+          <PageTitle pageTitle={pageTitle}/>
+        </Row>
+        {
+          nrProjects?.length > 0 ?
+            <Row>
+              <Col>
+              </Col>
+            </Row>
+          :
+            <Row className="mt-3 mb-3">
+              <Col className="d-flex align-items-center justify-content-center shadow-sm bg-light border border-danger rounded text-muted text-center p-3 fs-3" style={{height: '400px'}} md={{offset: 1, size: 10}}>
+                Nemate prijavljenih sudjelovanja na projektima
+              </Col>
+            </Row>
+        }
+      </>
+    )
 };
 
 export default Memberships;
