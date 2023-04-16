6d6ec7b6ee5b5c4e1d03314e03304797126d6ab3
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-04-16 16:11:31 +0200

    ensure proper project permissions on public key frontend

--- hrzoosignup/frontend/src/pages/public-keys/list.jsx
+++ hrzoosignup/frontend/src/pages/public-keys/list.jsx
@@ -24,6 +24,7 @@ import '../../styles/content.css';
 import ModalAreYouSure from '../../components/ModalAreYouSure';
 import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
+import { fetchNrProjects } from '../../api/projects';
 
 
 const PublicKeys = () => {
@@ -56,6 +57,11 @@ const PublicKeys = () => {
     if (showedKeys !== undefined)
       return showedKeys[keyname]
   }
+
+  const {status: nrStatus, data: nrProjects} = useQuery({
+      queryKey: ['projects'],
+      queryFn: fetchNrProjects
+  })
 
   const {status, data: sshKeysData, error, isFetching} = useQuery({
       queryKey: ['ssh-keys'],
@@ -109,185 +115,206 @@ const PublicKeys = () => {
     }
   }
 
-  if (status === 'success' && sshKeys?.length > 0)
-  {
-    return (
-      <>
-        <Row>
-          <PageTitle pageTitle={pageTitle}/>
-        </Row>
-        <ModalAreYouSure
-          isOpen={areYouSureModal}
-          toggle={() => setAreYouSureModal(!areYouSureModal)}
-          title={modalTitle}
-          msg={modalMsg}
-          onYes={onYesCallback} />
-        <Row className="mt-4 ms-4 me-4 mb-5">
-          <Col>
-            <Table responsive hover className="shadow-sm">
-              <thead id="hzsi-thead" className="table-active align-middle text-center text-white">
-                <tr className="border-bottom border-1 border-dark">
-                  <th className="fw-normal">
-                    Ime ključa
-                  </th>
-                  <th className="fw-normal">
-                    Digitalni otisak ključa
-                  </th>
-                  <th className="fw-normal">
-                    Tip
-                  </th>
-                  <th className="fw-normal">
-                    Radnje
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                { sshKeys.map((key, index) =>
-                  <>
-                    <tr key={index}>
-                      <td className="p-3 align-middle text-center">
-                        { key.name }
-                      </td>
-                      <td className="p-3 align-middle text-center font-monospace" style={{maxLength: '5'}}>
-                        { key.fingerprint }
-                      </td>
-                      <td className="align-middle text-center">
-                        { key.public_key.split(' ')[0] }
-                      </td>
-                      <td className="align-middle text-center">
-                        <Button size="sm" color="primary" onClick={() => showKey(key.name)}>
-                          <FontAwesomeIcon icon={faArrowDown} />
-                        </Button>
-                        <Button size="sm" className="ms-2" color="danger" onClick={() => {
-                          setAreYouSureModal(!areYouSureModal)
-                          setModalTitle("Brisanje javnog ključa")
-                          setModalMsg("Da li ste sigurni da želite obrisati javni ključ?")
-                          setOnYesCall('doremovesshkey')
-                          setOnYesCallArg(key.name)
-                        }}>
-                          <FontAwesomeIcon icon={faTimes} />
-                        </Button>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td  className="p-0 m-0" colSpan="4">
-                        <Collapse className="m-2 p-2" isOpen={isShowed(key.name)}>
-                          <Row>
-                            <Col sm={{size: 11}}>
-                              <InputGroup>
-                                <InputGroupText>
-                                  Javni ključ:
-                                </InputGroupText>
-                                <textarea
-                                  className="font-monospace form-control"
-                                  rows="5"
-                                  placeholder={
-                                    key.public_key
-                                  }
-                                />
-                              </InputGroup>
-                            </Col>
-                            <Col className="d-flex align-self-center align-content-center">
-                              <Button size="sm" className="ms-3" color="success">
-                                <FontAwesomeIcon icon={faCopy} />
-                              </Button>
-                            </Col>
-                          </Row>
-                        </Collapse>
-                      </td>
-                    </tr>
-                  </>
-                )}
-                {
-                  sshKeys.length < 5 && [...Array(5 - sshKeys.length)].map((e, i) =>
-                    <tr key={i + 5}>
-                      <td colSpan="4" style={{height: "40px"}}>
-                      </td>
-                    </tr>
-                  )
-                }
-              </tbody>
-            </Table>
-          </Col>
-        </Row>
-        <Row className="mb-5 mt-5">
-          <Col className="d-flex justify-content-center">
-            <Button color="success" onClick={() => {
-                navigate('novi')
-            }}>
-              <FontAwesomeIcon icon={faKey}/>{' '}
-              Dodaj javni ključ
-            </Button>
-          </Col>
-        </Row>
-      </>
+  if (nrStatus === 'success' && nrProjects) {
+    let projectsApproved = nrProjects.filter(project =>
+      project.state.name !== 'deny' && project.state.name !== 'submit'
     )
-  }
-  else if (status === "success" && sshKeys?.length === 0) {
-    return (
-      <>
-        <Row>
-          <PageTitle pageTitle={pageTitle}/>
-        </Row>
-        <Row className="mt-4 ms-4 me-4 mb-3">
-          <Col>
-            <Table responsive hover className="shadow-sm">
-              <thead id="hzsi-thead" className="table-active align-middle text-center text-white">
-                <tr className="border-bottom border-1 border-dark">
-                  <th className="fw-normal">
-                    Ime ključa
-                  </th>
-                  <th className="fw-normal">
-                    Digitalni otisak ključa
-                  </th>
-                  <th className="fw-normal">
-                    Tip
-                  </th>
-                  <th className="fw-normal">
-                    Radnje
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {
-                  [...Array(3)].map((_, i) => (
-                    <tr key={i}>
-                      <td colSpan="4" className="m-0 p-0 bg-light border-0">
-                        <Placeholder size="lg" xs={12} style={{height: '40px', backgroundColor: "rgba(255, 255, 255, 0)"}}/>
-                      </td>
-                    </tr>
-                  ))
-                }
-                <tr key="4">
-                  <td colSpan="4" className="table-light border-0 text-muted text-center p-3 fs-3">
-                    Nemate javnih ključeva dodanih
-                  </td>
-                </tr>
-                {
-                  [...Array(3)].map((_, i) => (
-                    <tr key={i + 6}>
-                      <td colSpan="4" className="m-0 p-0 bg-light border-0">
-                        <Placeholder size="lg" xs={12} style={{height: '40px', backgroundColor: "rgba(255, 255, 255, 0)"}}/>
-                      </td>
-                    </tr>
-                  ))
-                }
-              </tbody>
-            </Table>
-          </Col>
-        </Row>
-        <Row className="mb-2 mt-3">
-          <Col className="d-flex justify-content-center">
-            <Button color="success" onClick={() => {
-                navigate('novi')
-            }}>
-              <FontAwesomeIcon icon={faKey}/>{' '}
-              Dodaj javni ključ
-            </Button>
-          </Col>
-        </Row>
-      </>
-    )
+
+    if (projectsApproved.length === 0)
+      return (
+        <>
+          <Row>
+            <PageTitle pageTitle={pageTitle}/>
+          </Row>
+
+          <Row className="mt-3 mb-3">
+            <Col className="d-flex align-items-center justify-content-center shadow-sm bg-light border border-danger rounded text-muted text-center p-3 fs-3" style={{height: '400px'}} md={{offset: 1, size: 10}}>
+              Nemate prijavljenih sudjelovanja na odobrenim projektima
+            </Col>
+          </Row>
+        </>
+      )
+
+    else if (status === 'success' && sshKeys?.length > 0)
+    {
+      return (
+        <>
+          <Row>
+            <PageTitle pageTitle={pageTitle}/>
+          </Row>
+          <ModalAreYouSure
+            isOpen={areYouSureModal}
+            toggle={() => setAreYouSureModal(!areYouSureModal)}
+            title={modalTitle}
+            msg={modalMsg}
+            onYes={onYesCallback} />
+          <Row className="mt-4 ms-4 me-4 mb-5">
+            <Col>
+              <Table responsive hover className="shadow-sm">
+                <thead id="hzsi-thead" className="table-active align-middle text-center text-white">
+                  <tr className="border-bottom border-1 border-dark">
+                    <th className="fw-normal">
+                      Ime ključa
+                    </th>
+                    <th className="fw-normal">
+                      Digitalni otisak ključa
+                    </th>
+                    <th className="fw-normal">
+                      Tip
+                    </th>
+                    <th className="fw-normal">
+                      Radnje
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  { sshKeys.map((key, index) =>
+                    <>
+                      <tr key={index}>
+                        <td className="p-3 align-middle text-center">
+                          { key.name }
+                        </td>
+                        <td className="p-3 align-middle text-center font-monospace" style={{maxLength: '5'}}>
+                          { key.fingerprint }
+                        </td>
+                        <td className="align-middle text-center">
+                          { key.public_key.split(' ')[0] }
+                        </td>
+                        <td className="align-middle text-center">
+                          <Button size="sm" color="primary" onClick={() => showKey(key.name)}>
+                            <FontAwesomeIcon icon={faArrowDown} />
+                          </Button>
+                          <Button size="sm" className="ms-2" color="danger" onClick={() => {
+                            setAreYouSureModal(!areYouSureModal)
+                            setModalTitle("Brisanje javnog ključa")
+                            setModalMsg("Da li ste sigurni da želite obrisati javni ključ?")
+                            setOnYesCall('doremovesshkey')
+                            setOnYesCallArg(key.name)
+                          }}>
+                            <FontAwesomeIcon icon={faTimes} />
+                          </Button>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td  className="p-0 m-0" colSpan="4">
+                          <Collapse className="m-2 p-2" isOpen={isShowed(key.name)}>
+                            <Row>
+                              <Col sm={{size: 11}}>
+                                <InputGroup>
+                                  <InputGroupText>
+                                    Javni ključ:
+                                  </InputGroupText>
+                                  <textarea
+                                    className="font-monospace form-control"
+                                    rows="5"
+                                    placeholder={
+                                      key.public_key
+                                    }
+                                  />
+                                </InputGroup>
+                              </Col>
+                              <Col className="d-flex align-self-center align-content-center">
+                                <Button size="sm" className="ms-3" color="success">
+                                  <FontAwesomeIcon icon={faCopy} />
+                                </Button>
+                              </Col>
+                            </Row>
+                          </Collapse>
+                        </td>
+                      </tr>
+                    </>
+                  )}
+                  {
+                    sshKeys.length < 5 && [...Array(5 - sshKeys.length)].map((e, i) =>
+                      <tr key={i + 5}>
+                        <td colSpan="4" style={{height: "40px"}}>
+                        </td>
+                      </tr>
+                    )
+                  }
+                </tbody>
+              </Table>
+            </Col>
+          </Row>
+          <Row className="mb-5 mt-5">
+            <Col className="d-flex justify-content-center">
+              <Button color="success" onClick={() => {
+                  navigate('novi')
+              }}>
+                <FontAwesomeIcon icon={faKey}/>{' '}
+                Dodaj javni ključ
+              </Button>
+            </Col>
+          </Row>
+        </>
+      )
+    }
+    else if (status === "success" && sshKeys?.length === 0) {
+      return (
+        <>
+          <Row>
+            <PageTitle pageTitle={pageTitle}/>
+          </Row>
+          <Row className="mt-4 ms-4 me-4 mb-3">
+            <Col>
+              <Table responsive hover className="shadow-sm">
+                <thead id="hzsi-thead" className="table-active align-middle text-center text-white">
+                  <tr className="border-bottom border-1 border-dark">
+                    <th className="fw-normal">
+                      Ime ključa
+                    </th>
+                    <th className="fw-normal">
+                      Digitalni otisak ključa
+                    </th>
+                    <th className="fw-normal">
+                      Tip
+                    </th>
+                    <th className="fw-normal">
+                      Radnje
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {
+                    [...Array(3)].map((_, i) => (
+                      <tr key={i}>
+                        <td colSpan="4" className="m-0 p-0 bg-light border-0">
+                          <Placeholder size="lg" xs={12} style={{height: '40px', backgroundColor: "rgba(255, 255, 255, 0)"}}/>
+                        </td>
+                      </tr>
+                    ))
+                  }
+                  <tr key="4">
+                    <td colSpan="4" className="table-light border-0 text-muted text-center p-3 fs-3">
+                      Nemate javnih ključeva dodanih
+                    </td>
+                  </tr>
+                  {
+                    [...Array(3)].map((_, i) => (
+                      <tr key={i + 6}>
+                        <td colSpan="4" className="m-0 p-0 bg-light border-0">
+                          <Placeholder size="lg" xs={12} style={{height: '40px', backgroundColor: "rgba(255, 255, 255, 0)"}}/>
+                        </td>
+                      </tr>
+                    ))
+                  }
+                </tbody>
+              </Table>
+            </Col>
+          </Row>
+          <Row className="mb-2 mt-3">
+            <Col className="d-flex justify-content-center">
+              <Button color="success" onClick={() => {
+                  navigate('novi')
+              }}>
+                <FontAwesomeIcon icon={faKey}/>{' '}
+                Dodaj javni ključ
+              </Button>
+            </Col>
+          </Row>
+        </>
+      )
+    }
   }
 };
 
