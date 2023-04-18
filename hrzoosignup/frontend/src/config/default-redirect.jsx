c28abc6fb2b066ff02cf457bdd2dc2f0d6f8e4b7
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-04-18 11:13:06 +0200

    have real setting in redirection

--- hrzoosignup/frontend/src/config/default-redirect.jsx
+++ hrzoosignup/frontend/src/config/default-redirect.jsx
@@ -1,6 +1,6 @@
 import {url_ui_prefix} from './general.jsx';
 
-export const defaultUnAuthnRedirect = url_ui_prefix + '/prijava-priv'
+export const defaultUnAuthnRedirect = url_ui_prefix + '/prijava-pub'
 export const defaultAuthnRedirect = url_ui_prefix + '/moji-zahtjevi'
 export const defaultAuthnRedirectStaff = url_ui_prefix + '/upravljanje-zahtjevima'
 
