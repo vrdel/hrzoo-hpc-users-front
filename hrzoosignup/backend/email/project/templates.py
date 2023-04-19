843f6a0dcfbc43812659ca95c5d05a82e4c70ae3
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-04-19 16:24:11 +0200

    refine earlier emails

--- hrzoosignup/backend/email/project/templates.py
+++ hrzoosignup/backend/email/project/templates.py
@@ -3,44 +3,33 @@ from django.core.mail import send_mail
 
 
 def email_approve_project(to, name, prtype):
-    project_type_subject = ''
-    if prtype.name == 'thesis':
-        project_type_subject = "Završni rad/disertacija odobrena"
-    elif prtype.name == 'practical':
-        project_type_subject = "Praktična nastava odobrena"
-    elif prtype.name == 'research-croris':
-        project_type_subject = "Istraživački projekt odobren"
+    subject = '[Napredno računanje] Zahtjev odobren'
 
     email = \
 f"""\
 Poštovani/a,
 
-vaša je prijava projekta "{name}" pri usluzi Napredno računanje prihvaćena.
+vaš zahtjev za korištenje usluge Napredno računanje "{name}" je prihvaćen.
 
 {settings.EMAILSIGNATURE}
 """
 
     return send_mail(
-        project_type_subject,
+        subject,
         email,
         settings.EMAILFROM, to,
         fail_silently=True)
 
 
 def email_deny_project(to, name, prtype, comment):
-    project_type_subject = ''
-    if prtype.name == 'thesis':
-        project_type_subject = "Završni rad/disertacija nije prihvaćena"
-    elif prtype.name == 'practical':
-        project_type_subject = "Praktična nastava nije prihvaćena"
-    elif prtype.name == 'research-croris':
-        project_type_subject = "Istraživački projekt nije prihvaćen"
+    project_type_subject = '[Napredno računanje] Zahtjev nije odobren'
 
     email = \
 f"""\
 Poštovani/a,
 
-vaša je prijava zahtjeva "{name}" pri usluzi Napredno računanje odbačena, s obrazloženjem:
+vaš zahtjev za korištenje usluge Napredno računanje "{name}" je odbačen
+s obrazloženjem:
 
 {comment}
 
@@ -67,7 +56,7 @@ def email_new_project(to, name, lead, prtype, link):
 f"""\
 Poštovani/a,
 
-prijavljen je novi projekt pri usluzi Napredno računanje.
+prijavljen je novi projekt na usluzi Napredno računanje.
 
 Naziv: {name}
 
