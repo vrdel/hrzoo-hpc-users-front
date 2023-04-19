a6d6af35325dc9c70203e89ec30a6450ba35d3d3
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-04-19 14:19:12 +0200

    initial deny email

--- hrzoosignup/backend/email/project/templates.py
+++ hrzoosignup/backend/email/project/templates.py
@@ -24,7 +24,34 @@ vaša je prijava projekta "{name}" pri usluzi Napredno računanje prihvaćena.
         project_type_subject,
         email,
         settings.EMAILFROM, to,
-        fail_silently=False)
+        fail_silently=True)
+
+
+def email_deny_project(to, name, prtype):
+    project_type_subject = ''
+    if prtype.name == 'thesis':
+        project_type_subject = "Završni rad/disertacija odobrena"
+    elif prtype.name == 'practical':
+        project_type_subject = "Praktična nastava nije prihvaćen"
+    elif prtype.name == 'research-croris':
+        project_type_subject = "Istraživački projekt nije prihvaćen"
+
+    email = \
+f"""\
+Poštovani/a,
+
+vaša je prijava zahtjeva "{name}" pri usluzi Napredno računanje odbačena, s obrazloženjem:
+
+
+
+{settings.EMAILSIGNATURE}
+"""
+
+    return send_mail(
+        project_type_subject,
+        email,
+        settings.EMAILFROM, to,
+        fail_silently=True)
 
 
 def email_new_project(to, name, lead, prtype, link):
@@ -55,4 +82,4 @@ Pogledaj prijavu: https://computing.srce.hr/ui/upravljanje-zahtjevima/{link}
         'Prijava {}'.format(project_type_subject),
         email,
         settings.EMAILFROM, to,
-        fail_silently=False)
+        fail_silently=True)
