984da2a1a924d6dfdbb168254b3b9c9be11d1903
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-04-19 10:36:55 +0200

    proper fields for superuser

--- hrzoosignup/backend/management/commands/superuser.py
+++ hrzoosignup/backend/management/commands/superuser.py
@@ -19,16 +19,15 @@ class Command(BaseCommand):
         user_data = dict()
 
         user_data[self.user_model.USERNAME_FIELD] = settings.SUPERUSER_NAME
-        user_data['first_name'] = settings.SUPERUSER_NAME
-        user_data['last_name'] = settings.SUPERUSER_NAME
+        user_data['first_name'] = settings.SUPERUSER_FIRSTNAME
+        user_data['last_name'] = settings.SUPERUSER_LASTNAME
         user_data['password'] = settings.SUPERUSER_PASS
         user_data['email'] = settings.SUPERUSER_EMAIL
+        user_data['person_email'] = settings.SUPERUSER_EMAIL
 
         try:
             user = self.user_model._default_manager.db_manager(DEFAULT_DB_ALIAS).create_superuser(**user_data)
-            user.aaieduhr = None
-            user.institution = 'SRCE'
-            user.role = 'Superuser'
+            user.person_institution = 'SRCE'
             user.save()
             self.stdout.write("Superuser created successfully.")
         except IntegrityError:
