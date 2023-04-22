f2d0ff641173fcda758d2447203b79a0eb2f3a49
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-04-22 22:36:52 +0200

    added fetch API method for grabbing invites from backend

--- hrzoosignup/frontend/src/api/invite.jsx
+++ hrzoosignup/frontend/src/api/invite.jsx
@@ -45,6 +45,36 @@ export async function addInvite(data)
 }
 
 
+export async function fetchMyInvites()
+{
+  let error_msg = ''
+
+  try {
+    let response = await fetch(`${url_api_prefix}/api/v1/internal/invites-sent/`)
+
+    if (response.ok)
+      return await response.json()
+
+    if (!response.ok) {
+      try {
+        await response.json();
+        error_msg = `${response.status} ${response.statusText} in GET`
+      }
+      catch (err1) {
+        error_msg = `${response.status} ${response.statusText} in GET`
+      }
+    }
+
+  }
+  catch (err) {
+    error_msg = `${err} in GET`;
+  }
+
+  if (error_msg)
+    throw new Error(`Error fetching My Invites data: ${error_msg}`)
+}
+
+
 export async function fetchInvite(inviteKey)
 {
   let error_msg = ''
