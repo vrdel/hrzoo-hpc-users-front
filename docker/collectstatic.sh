2cbe3ab3023406454b977d7fe19508bf8767bcfc
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-05-11 18:00:21 +0200

    fix collectstatic

--- docker/collectstatic.sh
+++ docker/collectstatic.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 . /opt/hrzoo-signup/bin/activate
-rm -rf $VIRTUAL_ENV/usr/share/hrzoosignup/static/reactbundle/* ; \
+rm -rf $VIRTUAL_ENV/share/hrzoosignup/static/reactbundle/* ; \
 python $VIRTUAL_ENV/lib/python3.9/site-packages/hrzoosignup/manage.py collectstatic --noinput ; \
-rm -rf $VIRTUAL_ENV/usr/share/hrzoosignup/static/admin
+rm -rf $VIRTUAL_ENV/share/hrzoosignup/static/admin
