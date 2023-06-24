c152b7dc639255ff8683bedf7936f6b4b73b3c90
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-06-24 11:35:10 +0200

    if user initially wanted some path, extract from referrer localStorage and redirect to it after SAML2 login

--- hrzoosignup/frontend/src/pages/saml2-login-redirect.jsx
+++ hrzoosignup/frontend/src/pages/saml2-login-redirect.jsx
@@ -1,6 +1,10 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { defaultAuthnRedirect, defaultAuthnRedirectStaff } from '../config/default-redirect';
+import {
+  defaultUnAuthnRedirect,
+  defaultAuthnRedirect,
+  defaultAuthnRedirectStaff
+} from '../config/default-redirect';
 import { url_ui_prefix } from '../config/general';
 
 
@@ -9,14 +13,26 @@ const Saml2LoginRedirect = ({sessionData=undefined}) => {
 
   useEffect(() => {
     const inviteKey = localStorage.getItem('invitation-key-set')
-    const redir = sessionData.userdetails.is_staff || sessionData.userdetails.is_superuser
-      ? defaultAuthnRedirectStaff
-      : defaultAuthnRedirect
-    const origin = location.state?.from?.pathname || redir
     if (inviteKey)
       navigate(url_ui_prefix + '/prijava-email/' + inviteKey)
-    else
-      navigate(origin)
+    else {
+      let wantVisit = JSON.parse(localStorage.getItem('referrer'))
+      if (wantVisit) {
+        // before - defaultUnAuthnRedirect or path user initially requested
+        // last - path of this component
+        if (wantVisit.length >= 2)
+          wantVisit = wantVisit[wantVisit.length - 2]
+        const defaultRedirect = sessionData.userdetails.is_staff
+          || sessionData.userdetails.is_superuser
+          ? defaultAuthnRedirectStaff
+          : defaultAuthnRedirect
+        if (wantVisit !== defaultUnAuthnRedirect)
+          navigate(wantVisit)
+        else
+          navigate(defaultRedirect)
+      }
+      localStorage.removeItem('referrer')
+    }
   }, [location.pathname])
 
   return (
