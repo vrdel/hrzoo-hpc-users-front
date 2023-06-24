8651172d097a2b9a60a99377e73bb336a9e6a904
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-06-25 01:11:34 +0200

    badge for user mode navlinks

--- hrzoosignup/frontend/src/components/NavigationLinks.jsx
+++ hrzoosignup/frontend/src/components/NavigationLinks.jsx
@@ -26,12 +26,10 @@ import { AuthContext } from '../components/AuthContextProvider';
 import { elemInArray } from '../utils/array_help';
 
 
-const NavigationLinksUser = ({activeBgColor}) => {
-  const { userDetails } = useContext(AuthContext);
-
+const NavigationLinksUser = ({isAdmin, activeBgColor}) => {
   return (
     <>
-      <NavItem key='moji-zahtjevi' className={`${userDetails.is_staff ? 'mt-1' : 'ms-3 mt-1'}`}>
+      <NavItem key='moji-zahtjevi' className='ms-3 mt-1'>
         <NavLink
           style={({isActive}) => isActive ? {'backgroundColor': activeBgColor} : {}}
           className={({isActive}) => isActive ? "nav-link active text-white" : "nav-link text-dark"}
@@ -67,7 +65,7 @@ const NavigationLinksUser = ({activeBgColor}) => {
           Javni ključevi
         </NavLink>
       </NavItem>
-      <NavItem key='moji-podaci' className='mt-1 me-3 ms-auto'>
+      <NavItem key='moji-podaci' className={isAdmin ? 'mt-1 ms-auto' : 'mt-1 me-3 ms-auto'}>
         <NavLink
           style={({isActive}) => isActive ? {'backgroundColor': activeBgColor} : {}}
           className={({isActive}) => isActive ? "nav-link active text-white" : "nav-link text-dark"}
@@ -186,14 +184,12 @@ const NavigationLinksAdmin = ({activeBgColor}) => {
 const NavigationLinks = () => {
   const activeBgColor = '#b04c46';
   const { userDetails } = useContext(AuthContext);
-
   const [userMode, setUserMode] = useState(false);
-
 
   return (
     <Nav tabs id="hzsi-navlinks" className="border-start border-end rounded d-flex sticky-top">
       {
-        userDetails.is_staff || userDetails.is_superuser
+        (userDetails.is_staff || userDetails.is_superuser) && !userMode
         ?
           <>
             <NavigationLinksAdmin activeBgColor={activeBgColor} />
@@ -208,7 +204,25 @@ const NavigationLinks = () => {
             </NavItem>
           </>
         :
-          <NavigationLinksUser activeBgColor={activeBgColor} />
+          <>
+            <NavigationLinksUser
+              isAdmin={userDetails.is_staff || userDetails.is_superuser}
+              activeBgColor={activeBgColor}
+            />
+            {
+              (userDetails.is_staff || userDetails.is_superuser)
+              &&
+                <NavItem className='d-flex flex-column ms-3 me-3 justify-content-center'>
+                  <Badge
+                    size="sm"
+                    style={{cursor: 'pointer'}}
+                    onClick={() => setUserMode(!userMode)}
+                    color="success">
+                    user<br/>mode
+                  </Badge>
+                </NavItem>
+            }
+          </>
       }
     </Nav>
   )
