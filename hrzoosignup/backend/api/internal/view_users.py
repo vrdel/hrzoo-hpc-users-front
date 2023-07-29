01878256cc717dc0eb6e1f78984496edf05c4235
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-07-29 08:25:30 +0200

    active-users API call only for is_staff and is_superuser

--- hrzoosignup/backend/api/internal/view_users.py
+++ hrzoosignup/backend/api/internal/view_users.py
@@ -18,7 +18,7 @@ class UsersInfoOps(APIView):
             extract_users = []
             ret_data = cache.get('usersinfo-ops-get')
             if ret_data:
-                return Response(ret_data)
+                return Response(ret_data, status=status.HTTP_200_OK)
 
             users = get_user_model().objects.filter(is_staff=True)
             for user in users:
@@ -47,50 +47,60 @@ class UsersInfo(APIView):
     permission_classes = (IsAuthenticated,)
 
     def get(self, request):
-        ret_data = cache.get('usersinfo-get')
-        if ret_data:
+        if request.user.is_staff or request.user.is_superuser:
+            ret_data = cache.get('usersinfo-get')
+            if ret_data:
+                return Response(ret_data, status=status.HTTP_200_OK)
+
+            users = models.User.objects.all()
+            lead = models.Role.objects.get(name="lead")
+            collaborator = models.Role.objects.get(name="collaborator")
+
+            resp_users = list()
+            for user in users:
+                userprojects = models.UserProject.objects.filter(user=user)
+                active_userprojects = [
+                    p for p in userprojects if p.project.state.name == "approve"
+                ]
+                projects_lead = sorted(
+                    [up for up in active_userprojects if up.role == lead],
+                    key=lambda k: k.project.identifier
+                )
+                projects_collab = sorted(
+                    [up for up in active_userprojects if up.role == collaborator],
+                    key=lambda k: k.project.identifier
+                )
+                projects = projects_lead + projects_collab
+                ssh_keys = len(models.SSHPublicKey.objects.filter(user=user))
+                if len(projects) > 0:
+                    resp_users.append({
+                        "username": user.username,
+                        "first_name": user.first_name,
+                        "last_name": user.last_name,
+                        "person_institution": user.person_institution,
+                        "person_mail": user.person_mail,
+                        "ssh_key": ssh_keys > 0,
+                        "n_ssh_key": ssh_keys,
+                        "projects": [{
+                            "identifier": userproject.project.identifier,
+                            "state": userproject.project.state.name,
+                            "role": userproject.role.name,
+                            "type": userproject.project.project_type.name
+                        } for userproject in projects],
+                        "date_joined":
+                            user.date_joined.strftime("%Y-%m-%d %H:%M:%S")
+                    })
+
+                ret_data = sorted(resp_users, key=lambda k: k["first_name"])
+                cache.set('usersinfo-get', ret_data, 60 * 15)
+
             return Response(ret_data)
 
-        users = models.User.objects.all()
-        lead = models.Role.objects.get(name="lead")
-        collaborator = models.Role.objects.get(name="collaborator")
-
-        resp_users = list()
-        for user in users:
-            userprojects = models.UserProject.objects.filter(user=user)
-            active_userprojects = [
-                p for p in userprojects if p.project.state.name == "approve"
-            ]
-            projects_lead = sorted(
-                [up for up in active_userprojects if up.role == lead],
-                key=lambda k: k.project.identifier
-            )
-            projects_collab = sorted(
-                [up for up in active_userprojects if up.role == collaborator],
-                key=lambda k: k.project.identifier
-            )
-            projects = projects_lead + projects_collab
-            ssh_keys = len(models.SSHPublicKey.objects.filter(user=user))
-            if len(projects) > 0:
-                resp_users.append({
-                    "username": user.username,
-                    "first_name": user.first_name,
-                    "last_name": user.last_name,
-                    "person_institution": user.person_institution,
-                    "person_mail": user.person_mail,
-                    "ssh_key": ssh_keys > 0,
-                    "n_ssh_key": ssh_keys,
-                    "projects": [{
-                        "identifier": userproject.project.identifier,
-                        "state": userproject.project.state.name,
-                        "role": userproject.role.name,
-                        "type": userproject.project.project_type.name
-                    } for userproject in projects],
-                    "date_joined":
-                        user.date_joined.strftime("%Y-%m-%d %H:%M:%S")
-                })
-
-            ret_data = sorted(resp_users, key=lambda k: k["first_name"])
-            cache.set('usersinfo-get', ret_data, 60 * 15)
-
-        return Response(ret_data)
+        else:
+            err_response = {
+                'status': {
+                    'code': status.HTTP_401_UNAUTHORIZED,
+                    'message': '{} - Not allowed to view the active users'.format(request.user.username)
+                }
+            }
+            return Response(err_response, status=status.HTTP_401_UNAUTHORIZED)
