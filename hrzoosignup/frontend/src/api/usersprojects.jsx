042ba2f69851ea75869dc51fe21170b53bec5dae
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-10-03 22:55:37 +0200

    add slipped fetch API method from frontend that POST on users-projects

---
+++ hrzoosignup/frontend/src/api/usersprojects.jsx
@@ -0,0 +1,42 @@
+import { url_api_prefix } from '../config/general';
+
+
+export async function removeUserFromProject(projectId, data, csrftoken)
+{
+  let error_msg = ''
+
+  console.log('VRDEL DEBUG', projectId, data, csrftoken)
+  try {
+    let response = await fetch(`${url_api_prefix}/api/v1/internal/users-projects/${projectId}`, {
+      method: 'POST',
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'same-origin',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'X-CSRFToken': csrftoken,
+        'Referer': 'same-origin'
+      },
+      body: data && JSON.stringify(data)
+    })
+
+    if (!response.ok) {
+      try {
+        let json = await response.json();
+        error_msg = `${response.status} ${response.statusText} in POST: ${json?.status?.message}`
+      }
+      catch (err1) {
+        error_msg = `${response.status} ${response.statusText} in POST`
+      }
+    }
+
+    return response
+  }
+  catch (err) {
+    error_msg = `${err} in POST`;
+  }
+
+  if (error_msg)
+    throw new Error(`Error removing users from project: ${error_msg}`)
+}
