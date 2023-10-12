c416fb6afa8b2b67b44146583b9ba2b373a5d4ae
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-10-12 15:55:20 +0200

    call flush from parent class and implement deletion of two additional Django models

--- hrzoosignup/backend/management/commands/myflush.py
+++ hrzoosignup/backend/management/commands/myflush.py
@@ -1,4 +1,30 @@
 from django.core.management.commands.flush import Command as FlushCommand
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import Permission
 
 class Command(FlushCommand):
-    pass
+    def add_arguments(self, parser):
+        super(Command, self).add_arguments(parser)
+        parser.add_argument(
+            "--yes",
+            action="store_true",
+            dest="confirmed_yes",
+            help="Tells Django to NOT prompt the user for input of any kind.",
+        )
+        pass
+
+    def handle(self, **options):
+        self.stdout.write(self.style.WARNING('Trying to completely flush database'))
+
+        if options.get('confirmed_yes', None):
+            options['interactive'] = False
+        else:
+            self.stdout.write(self.style.ERROR('Run with explicit --yes argument'))
+            raise SystemExit(1)
+
+        super(Command, self).handle(**options)
+        self.stdout.write(self.style.SUCCESS('Application tables deleted'))
+
+        ContentType.objects.all().delete()
+        Permission.objects.all().delete()
+        self.stdout.write(self.style.SUCCESS('Django tables deleted'))
