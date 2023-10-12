e0e03b6de76892b52203942f652695c4b7182a5a
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-10-12 23:16:10 +0200

    initial filter by one tag

--- hrzoosignup/backend/api/view_userproject.py
+++ hrzoosignup/backend/api/view_userproject.py
@@ -16,13 +16,27 @@ class UserProjectAPI(APIView):
         tags = self.request.query_params.get('tags')
 
         if tags:
+            ret_data = cache.get('ext-users-projects')
+
+            if ret_data:
+                return Response(ret_data, status=status.HTTP_200_OK)
+
+            for tag in tags:
+            serializer = \
+                serializers.UserProjectSerializer2(models.UserProject.objects.filter(
+                    project__staff_resources_type__contains=[{"label": tags, "value": tags}]), many=True
+                )
             import ipdb; ipdb.set_trace()
+            cache.set('ext-users-projects', serializer.data, 60 * 15)
 
-        ret_data = cache.get('ext-users-projects')
-        if ret_data:
-            return Response(ret_data, status=status.HTTP_200_OK)
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
-        serializer = serializers.UserProjectSerializer2(models.UserProject.objects.all(), many=True)
-        cache.set('ext-users-projects', serializer.data, 60 * 15)
+        else:
+            ret_data = cache.get('ext-users-projects')
+            if ret_data:
+                return Response(ret_data, status=status.HTTP_200_OK)
 
-        return Response(serializer.data, status=status.HTTP_200_OK)
+            serializer = serializers.UserProjectSerializer2(models.UserProject.objects.all(), many=True)
+            cache.set('ext-users-projects', serializer.data, 60 * 15)
+
+            return Response(serializer.data, status=status.HTTP_200_OK)
