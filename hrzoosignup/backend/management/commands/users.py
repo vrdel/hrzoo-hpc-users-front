c6affe748753d4dab7132a3c34150713d64fc1ff
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-10-27 13:16:30 +0200

    refactored with separate methods

--- hrzoosignup/backend/management/commands/users.py
+++ hrzoosignup/backend/management/commands/users.py
@@ -24,6 +24,171 @@ class Command(BaseCommand):
         super().__init__()
         self.user_model = get_user_model()
 
+    def _user_create(self, options):
+        user, project = None, None
+
+        if options['project']:
+            try:
+                project = Project.objects.get(identifier=options['project'])
+                self.stdout.write('Found project {} \"{}\"'.format(options['project'], project.name))
+
+            except Project.DoesNotExist as exc:
+                self.stdout.write(self.style.ERROR('Project does not exist'))
+                self.stdout.write(self.style.NOTICE(repr(exc)))
+                raise SystemExit(1)
+
+        try:
+            user = self.user_model.objects.create(
+                username=options['username'],
+                first_name=options['first'],
+                last_name=options['last'],
+                person_uniqueid=options['uniqueid'],
+                person_mail=options['email'],
+                croris_first_name=options['first'],
+                croris_last_name=options['last'],
+                croris_mail=options['email'],
+                person_oib=options['oib'],
+                person_institution=options['institution'],
+                person_organisation=options['organisation'],
+            )
+            self.stdout.write('Created user {}'.format(user.username))
+            cache.delete("usersinfoinactive-get")
+            cache.delete("usersinfo-get")
+
+        except IntegrityError as exc:
+            self.stdout.write(self.style.ERROR('Error creating user'))
+            self.stdout.write(self.style.NOTICE(repr(exc)))
+            raise SystemExit(1)
+
+        if options['key'] or options['keyname']:
+            if not (options['key'] and options['keyname']):
+                self.stdout.write(self.style.NOTICE('Both key path and key name should be specified'))
+                raise SystemExit(1)
+
+            key_content = options['key'].read().strip()
+            serializer = SshKeysSerializer(data={
+                'name': options['keyname'],
+                'public_key': key_content,
+                'user': user.id
+            })
+
+            if serializer.is_valid():
+                serializer.save()
+                self.stdout.write('Added key {} for the user {}'.format(
+                    serializer.data['fingerprint'], user.username))
+                cache.delete("ext-sshkeys")
+            else:
+                self.stdout.write(self.style.ERROR('Error adding key for the user'))
+                self.stdout.write(self.style.NOTICE(repr(serializer.errors)))
+                raise SystemExit(1)
+
+        if project and user:
+            try:
+                role_colab = Role.objects.get(name='collaborator')
+                UserProject.objects.create(
+                    user=user,
+                    project=project,
+                    role=role_colab,
+                    date_joined=timezone.make_aware(datetime.datetime.now())
+                )
+                self.stdout.write('User {} assigned to project {}'.format(user.username, project.identifier))
+                cache.delete("ext-users-projects")
+                cache.delete('projects-get-all')
+
+            except IntegrityError as exc:
+                self.stdout.write(self.style.ERROR('Error assigning user {} to project {}'.format(user.username, project.identifier)))
+                self.stdout.write(self.style.NOTICE(repr(exc)))
+                raise SystemExit(1)
+
+    def _user_update(self, options):
+        user, project = None, None
+
+        try:
+            user = self.user_model.objects.get(
+                username=options['username'],
+            )
+
+        except self.user_model.DoesNotExist as exc:
+            self.stdout.write(self.style.ERROR('User not found'))
+            self.stdout.write(self.style.NOTICE(repr(exc)))
+            raise SystemExit(1)
+
+        if options['project']:
+            try:
+                project = Project.objects.get(identifier=options['project'])
+                role_colab = Role.objects.get(name='collaborator')
+                self.stdout.write('Found project {} \"{}\"'.format(options['project'], project.name))
+                UserProject.objects.create(
+                    user=user,
+                    project=project,
+                    role=role_colab,
+                    date_joined=timezone.make_aware(datetime.datetime.now())
+                )
+                self.stdout.write('User {} assigned to project {}'.format(user.username, project.identifier))
+                cache.delete("ext-users-projects")
+                cache.delete('projects-get-all')
+
+            except Project.DoesNotExist as exc:
+                self.stdout.write(self.style.ERROR('Project does not exist'))
+                self.stdout.write(self.style.NOTICE(repr(exc)))
+                raise SystemExit(1)
+
+            except IntegrityError as exc:
+                self.stdout.write(self.style.ERROR('Error assigning user {} to project {}'.format(user.username, project.identifier)))
+                self.stdout.write(self.style.NOTICE(repr(exc)))
+                raise SystemExit(1)
+
+    def _user_delete(self, options):
+        try:
+            user = self.user_model.objects.get(
+                username=options['username']
+            )
+
+            # if SSH key specified, remove only it
+            # without removing the user
+            if options['keyname']:
+                sshkey = SSHPublicKey.objects.get(
+                    name=options['keyname'],
+                    user=user
+                )
+                if sshkey:
+                    sshkey.delete()
+                    self.stdout.write('Deleted key {} user {}'.format(sshkey.fingerprint, user.username))
+                    cache.delete("ext-sshkeys")
+                    raise SystemExit(0)
+
+            # if project specified, unsign user from it
+            elif options['project']:
+                try:
+                    userproject = UserProject.objects.get(
+                        project__identifier=options['project'],
+                        user=user
+                    )
+                    userproject.delete()
+                    self.stdout.write('Removed user {} from project {}'.format(user.username, options['project']))
+                    raise SystemExit(0)
+
+                except UserProject.DoesNotExist as exc:
+                    self.stdout.write(self.style.ERROR('Error removing user from project'))
+                    self.stdout.write(self.style.NOTICE(repr(exc)))
+                    raise SystemExit(1)
+            else:
+                user.delete()
+                cache.delete("usersinfoinactive-get")
+                cache.delete("usersinfo-get")
+                cache.delete("ext-users-projects")
+                cache.delete('projects-get-all')
+                self.stdout.write('Deleted user {}'.format(user.username))
+
+        except SSHPublicKey.DoesNotExist as exc:
+            self.stdout.write(self.style.ERROR('Error deleting SSH key for user'))
+            self.stdout.write(self.style.NOTICE(repr(exc)))
+
+        except self.user_model.DoesNotExist as exc:
+            self.stdout.write(self.style.ERROR('Error deleting user'))
+            self.stdout.write(self.style.NOTICE(repr(exc)))
+            raise SystemExit(1)
+
     def add_arguments(self, parser):
         subparsers = parser.add_subparsers(help="User subcommands", dest="command")
         parser_create = subparsers.add_parser("create", help="Create user based on passed metadata")
@@ -73,169 +238,11 @@ class Command(BaseCommand):
                                    required=False, help='Project identifier that user will be assigned to')
 
     def handle(self, *args, **options):
-        user_model = get_user_model()
-
         if options['command'] == 'delete':
-            try:
-                user = user_model.objects.get(
-                    username=options['username']
-                )
-
-                # if SSH key specified, remove only it
-                # without removing the user
-                if options['keyname']:
-                    sshkey = SSHPublicKey.objects.get(
-                        name=options['keyname'],
-                        user=user
-                    )
-                    if sshkey:
-                        sshkey.delete()
-                        self.stdout.write('Deleted key {} user {}'.format(sshkey.fingerprint, user.username))
-                        cache.delete("ext-sshkeys")
-                        raise SystemExit(0)
-
-                # if project specified, unsign user from it
-                elif options['project']:
-                    try:
-                        userproject = UserProject.objects.get(
-                            project__identifier=options['project'],
-                            user=user
-                        )
-                        userproject.delete()
-                        self.stdout.write('Removed user {} from project {}'.format(user.username, options['project']))
-                        raise SystemExit(0)
-
-                    except UserProject.DoesNotExist as exc:
-                        self.stdout.write(self.style.ERROR('Error removing user from project'))
-                        self.stdout.write(self.style.NOTICE(repr(exc)))
-                        raise SystemExit(1)
-                else:
-                    user.delete()
-                    cache.delete("usersinfoinactive-get")
-                    cache.delete("usersinfo-get")
-                    cache.delete("ext-users-projects")
-                    cache.delete('projects-get-all')
-                    self.stdout.write('Deleted user {}'.format(user.username))
-
-            except SSHPublicKey.DoesNotExist as exc:
-                self.stdout.write(self.style.ERROR('Error deleting SSH key for user'))
-                self.stdout.write(self.style.NOTICE(repr(exc)))
-
-            except user_model.DoesNotExist as exc:
-                self.stdout.write(self.style.ERROR('Error deleting user'))
-                self.stdout.write(self.style.NOTICE(repr(exc)))
-                raise SystemExit(1)
+            self._user_delete(options)
 
         if options['command'] == 'create':
-            user, project = None, None
-
-            if options['project']:
-                try:
-                    project = Project.objects.get(identifier=options['project'])
-                    self.stdout.write('Found project {} \"{}\"'.format(options['project'], project.name))
-
-                except Project.DoesNotExist as exc:
-                    self.stdout.write(self.style.ERROR('Project does not exist'))
-                    self.stdout.write(self.style.NOTICE(repr(exc)))
-                    raise SystemExit(1)
-
-            try:
-                user = user_model.objects.create(
-                    username=options['username'],
-                    first_name=options['first'],
-                    last_name=options['last'],
-                    person_uniqueid=options['uniqueid'],
-                    person_mail=options['email'],
-                    croris_first_name=options['first'],
-                    croris_last_name=options['last'],
-                    croris_mail=options['email'],
-                    person_oib=options['oib'],
-                    person_institution=options['institution'],
-                    person_organisation=options['organisation'],
-                )
-                self.stdout.write('Created user {}'.format(user.username))
-                cache.delete("usersinfoinactive-get")
-                cache.delete("usersinfo-get")
-
-            except IntegrityError as exc:
-                self.stdout.write(self.style.ERROR('Error creating user'))
-                self.stdout.write(self.style.NOTICE(repr(exc)))
-                raise SystemExit(1)
-
-            if options['key'] or options['keyname']:
-                if not (options['key'] and options['keyname']):
-                    self.stdout.write(self.style.NOTICE('Both key path and key name should be specified'))
-                    raise SystemExit(1)
-
-                key_content = options['key'].read().strip()
-                serializer = SshKeysSerializer(data={
-                    'name': options['keyname'],
-                    'public_key': key_content,
-                    'user': user.id
-                })
-
-                if serializer.is_valid():
-                    serializer.save()
-                    self.stdout.write('Added key {} for the user {}'.format(
-                        serializer.data['fingerprint'], user.username))
-                    cache.delete("ext-sshkeys")
-                else:
-                    self.stdout.write(self.style.ERROR('Error adding key for the user'))
-                    self.stdout.write(self.style.NOTICE(repr(serializer.errors)))
-                    raise SystemExit(1)
-
-            if project and user:
-                try:
-                    role_colab = Role.objects.get(name='collaborator')
-                    UserProject.objects.create(
-                        user=user,
-                        project=project,
-                        role=role_colab,
-                        date_joined=timezone.make_aware(datetime.datetime.now())
-                    )
-                    self.stdout.write('User {} assigned to project {}'.format(user.username, project.identifier))
-                    cache.delete("ext-users-projects")
-                    cache.delete('projects-get-all')
-
-                except IntegrityError as exc:
-                    self.stdout.write(self.style.ERROR('Error assigning user {} to project {}'.format(user.username, project.identifier)))
-                    self.stdout.write(self.style.NOTICE(repr(exc)))
-                    raise SystemExit(1)
+            self._user_create(options)
 
         if options['command'] == 'update':
-            user, project = None, None
-
-            try:
-                user = user_model.objects.get(
-                    username=options['username'],
-                )
-
-            except user_model.DoesNotExist as exc:
-                self.stdout.write(self.style.ERROR('User not found'))
-                self.stdout.write(self.style.NOTICE(repr(exc)))
-                raise SystemExit(1)
-
-            if options['project']:
-                try:
-                    project = Project.objects.get(identifier=options['project'])
-                    role_colab = Role.objects.get(name='collaborator')
-                    self.stdout.write('Found project {} \"{}\"'.format(options['project'], project.name))
-                    UserProject.objects.create(
-                        user=user,
-                        project=project,
-                        role=role_colab,
-                        date_joined=timezone.make_aware(datetime.datetime.now())
-                    )
-                    self.stdout.write('User {} assigned to project {}'.format(user.username, project.identifier))
-                    cache.delete("ext-users-projects")
-                    cache.delete('projects-get-all')
-
-                except Project.DoesNotExist as exc:
-                    self.stdout.write(self.style.ERROR('Project does not exist'))
-                    self.stdout.write(self.style.NOTICE(repr(exc)))
-                    raise SystemExit(1)
-
-                except IntegrityError as exc:
-                    self.stdout.write(self.style.ERROR('Error assigning user {} to project {}'.format(user.username, project.identifier)))
-                    self.stdout.write(self.style.NOTICE(repr(exc)))
-                    raise SystemExit(1)
+            self._user_update(options)
