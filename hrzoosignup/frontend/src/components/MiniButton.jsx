378f538b631f17b9fe9e85b51785234eeeb47435
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-10-30 17:55:15 +0100

    pass className to underlying component

--- hrzoosignup/frontend/src/components/MiniButton.jsx
+++ hrzoosignup/frontend/src/components/MiniButton.jsx
@@ -4,9 +4,9 @@ import {
 } from "reactstrap";
 
 
-export const MiniButton = ({onClick, children}) => (
+export const MiniButton = ({onClick, childClassName='', children}) => (
   <Button
-    className="d-flex align-items-center justify-content-center ms-1 ps-1 pe-1 pt-1 pb-1 mt-0"
+    className={`d-flex align-items-center justify-content-center ms-1 ps-1 pe-1 pt-1 pb-1 mt-0 ${childClassName}`}
     onClick={onClick}
     color="light">
     {children}
