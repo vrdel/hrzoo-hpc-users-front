2c179461bebe377e93a0fcd421bf94db429c535f
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-10-31 21:02:35 +0100

    added badge definition for new project type

--- hrzoosignup/frontend/src/config/map-projecttypes.jsx
+++ hrzoosignup/frontend/src/config/map-projecttypes.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 
+
 export function TypeString(type_name) {
   let type2string = {
     'research-croris': <>Istraživački<br/>projekt</>,
     'research-institutional': <>Institucijski<br/>projekt</>,
     'thesis': <>Izrada<br/>rada</>,
     'practical': <>Praktična<br/>nastava</>,
+    'internal': <>Interni<br/>projekt</>,
   }
 
   return type2string[type_name]
 }
+
 
 export function TypeColor(type_name) {
   let type2string = {
@@ -17,6 +20,7 @@ export function TypeColor(type_name) {
     'research-institutional': "bg-danger",
     'thesis': "bg-primary",
     'practical': "bg-warning text-dark",
+    'internal': "bg-secondary text-white",
   }
 
   return type2string[type_name]
