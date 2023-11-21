6e6c72cba58e158060b50133eb5c70f26b9759a4
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-11-21 17:30:54 +0100

    api/userprojects can take projects URL parameter

--- hrzoosignup/backend/api/view_userproject.py
+++ hrzoosignup/backend/api/view_userproject.py
@@ -15,6 +15,7 @@ class UserProjectAPI(APIView):
 
     def get(self, request):
         tags = self.request.query_params.get('tags')
+        projects = self.request.query_params.get('projects')
         query = Q()
         cached_interested, db_interested = list(), list()
 
@@ -30,6 +31,27 @@ class UserProjectAPI(APIView):
             else:
                 for tag in tags:
                     query |= Q(project__staff_resources_type__contains=[{"label": tag, "value": tag}])
+                db_interested = models.UserProject.objects.filter(query).distinct()
+
+            if cached_interested:
+                return Response(cached_interested, status=status.HTTP_200_OK)
+            else:
+                serializer = \
+                    serializers.UserProjectSerializer2(db_interested, many=True)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+
+        elif projects:
+            projects = projects.split(',')
+            cached_data = cache.get('ext-users-projects')
+
+            if cached_data:
+                for project in projects:
+                    for up in cached_data:
+                        if project in up['project']['identifier']:
+                            cached_interested.append(up)
+            else:
+                for project in projects:
+                    query |= Q(project__identifier=project)
                 db_interested = models.UserProject.objects.filter(query).distinct()
 
             if cached_interested:
