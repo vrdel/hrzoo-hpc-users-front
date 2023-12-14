6da4a355431d8823d908350f86934717c08a789c
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-12-14 15:57:47 +0100

    initial mailinglist_subscribe task

--- hrzoosignup/backend/management/commands/mailinglist_subscribe.py
+++ hrzoosignup/backend/management/commands/mailinglist_subscribe.py
@@ -1,12 +1,12 @@
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth.models import Permission
-from django.conf import settings
 from django.core.cache import cache
 
 
 from backend.httpq.httpconn import SessionWithRetry
 from backend.httpq.excep import HZSIHttpError
+from backend.tasks.mailinglist_subscribe import ListSubscribe
 
 
 class Command(BaseCommand):
@@ -19,6 +19,13 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         all_users = self.user_model.objects.all()
 
+        users_to_subscribe = list()
+
         for user in all_users:
             if user.status and not user.mailinglist_subscribe:
                 self.stdout.write(self.style.SUCCESS(f'Subscribing user {user.username} to mailing list'))
+                users_to_subscribe.append(user)
+
+        ListSubscribe(users_to_subscribe).run()
+
+
