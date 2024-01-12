7e0f9b823c147d29491dc1d1ad4c7b3569721209
Daniel Vrcic <daniel.vrcic@gmail.com>
2024-01-12 18:43:01 +0100

    proper construct of FetchAPI error message from backend

--- hrzoosignup/frontend/src/api/croris.jsx
+++ hrzoosignup/frontend/src/api/croris.jsx
@@ -2,12 +2,28 @@ import { url_api_prefix } from '../config/general';
 
 export async function fetchCroRIS()
 {
+  let error_msg = ''
+
   try {
     let response = await fetch(`${url_api_prefix}/api/v1/internal/croris-info/`)
+
     if (response.ok)
       return response.json()
-  } catch (err) {
-    throw new Error(`Error fetching data from CroRIS: ${err.message}`)
-  }
-}
 
+    if (!response.ok) {
+      try {
+        await response.text().then(text => {throw new Error(text)})
+      }
+      catch (err1) {
+        error_msg = `${err1.message}`
+      }
+    }
+  }
+  catch (err) {
+    error_msg = `${err} in GET`;
+  }
+
+  if (error_msg)
+    throw new Error(`Error fetching data from CroRIS: ${error_msg}`)
+
+}
