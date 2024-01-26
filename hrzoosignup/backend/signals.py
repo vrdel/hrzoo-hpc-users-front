35230b5648eaadf10ac124f23889947f2b8f869b
Daniel Vrcic <daniel.vrcic@gmail.com>
2024-01-26 09:46:36 +0100

    generate username for User on new UserProject tuple if not existing

--- hrzoosignup/backend/signals.py
+++ hrzoosignup/backend/signals.py
@@ -1,11 +1,16 @@
 from django.db.models.signals import post_save
 from .models import UserProject
+from backend.utils.gen_username import gen_username
 
 
 def generate_username(sender, instance, created, **kwargs):
-
     if kwargs['raw']:
         return
+
+    if not instance.user.person_username:
+        new_username = gen_username(instance.user.first_name, instance.user.last_name)
+        instance.user.person_username = new_username
+        instance.user.save()
 
 
 post_save.connect(generate_username, sender=UserProject)
--- hrzoosignup/backend/utils/gen_username.py
+++ hrzoosignup/backend/utils/gen_username.py
@@ -1,4 +1,6 @@
 from unidecode import unidecode
+from django.conf import settings
+from django.contrib.auth import get_user_model
 
 
 def only_alnum(s: str) -> str:
@@ -12,19 +14,20 @@ def only_alnum(s: str) -> str:
     return s
 
 
-def gen_username(first, last, existusers):
+def gen_username(first, last):
     # ASCII convert
     name = only_alnum(unidecode(first.lower()))
     surname = only_alnum(unidecode(last.lower()))
     # take first char of name and first seven from surname
     username = name[0] + surname[:7]
+    existing_usernames = get_user_model().objects.all().values_list('person_username', flat=True)
 
-    if username in existusers:
+    if username in existing_usernames:
         match = list()
         if len(username) < 8:
-            match = list(filter(lambda u: u.startswith(username), existusers))
+            match = list(filter(lambda u: u.startswith(username), existing_usernames))
         else:
-            match = list(filter(lambda u: u.startswith(username[:-1]), existusers))
+            match = list(filter(lambda u: u.startswith(username[:-1]), existing_usernames))
 
         return username + str(len(match))
 
