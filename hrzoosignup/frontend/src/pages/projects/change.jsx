1d82f1f1322e7185d64ca6f4780e77b146f5dfe5
Daniel Vrcic <daniel.vrcic@gmail.com>
2024-02-13 17:20:11 +0100

    ProjectChange component that actually wraps ManageRequestsChange

---
+++ hrzoosignup/frontend/src/pages/projects/change.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { ManageRequestsChange } from 'Pages/manage-requests/change';
+
+
+export const ProjectChange = () =>
+  <ManageRequestsChange manageProject={true} />
+
+
+export default ProjectChange;
