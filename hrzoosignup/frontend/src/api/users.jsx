4ff1330558c7dc3adda13581b05efce7f3f3b5b6
Daniel Vrcic <daniel.vrcic@gmail.com>
2024-02-19 13:29:08 +0100

    introduce fetchSpecific Fetch API method

--- hrzoosignup/frontend/src/api/users.jsx
+++ hrzoosignup/frontend/src/api/users.jsx
@@ -1,5 +1,4 @@
-
-import { url_api_prefix } from '../config/general';
+import { url_api_prefix } from 'Config/general';
 
 
 export async function fetchUsersInactive() {
@@ -32,6 +31,31 @@ export async function fetchUsers() {
 
 	try {
 		let response = await fetch(`${url_api_prefix}/api/v1/internal/active-users/`)
+
+		if (response.ok) {
+			return await response.json()
+		} else {
+			try {
+				await response.json()
+				error_msg = `${response.status} ${response.statusText} in GET`
+			} catch (err1) {
+				error_msg = `${response.status} ${response.statusText} in GET`
+			}
+		}
+	} catch(err) {
+		error_msg = `${err} in GET`
+	}
+
+	if (error_msg)
+		throw new Error(`Error fetching users data: ${error_msg}`)
+}
+
+
+export async function fetchSpecificUser(username) {
+	let error_msg = ""
+
+	try {
+		let response = await fetch(`${url_api_prefix}/api/v1/internal/users/${username}`)
 
 		if (response.ok) {
 			return await response.json()
