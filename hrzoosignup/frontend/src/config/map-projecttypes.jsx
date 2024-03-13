c516030c52dd58ecb8f073357ab33406c5c6d80d
Daniel Vrcic <daniel.vrcic@gmail.com>
2024-03-13 18:10:15 +0100

    style badge for new request type

--- hrzoosignup/frontend/src/config/map-projecttypes.jsx
+++ hrzoosignup/frontend/src/config/map-projecttypes.jsx
@@ -8,6 +8,7 @@ export function TypeString(type_name) {
     'thesis': <>Izrada<br/>rada</>,
     'practical': <>Praktična<br/>nastava</>,
     'internal': <>Interni<br/>projekt</>,
+    'srce-workshop': <>Srce<br/>radionica</>,
   }
 
   return type2string[type_name]
@@ -18,6 +19,7 @@ export function TypeColor(type_name) {
   let type2string = {
     'research-croris': "bg-success",
     'research-institutional': "bg-danger",
+    'srce-workshop': "bg-dark",
     'thesis': "bg-primary",
     'practical': "bg-warning text-dark",
     'internal': "bg-info text-dark",
