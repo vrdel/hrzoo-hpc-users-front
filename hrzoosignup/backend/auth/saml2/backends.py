99d61f3d61a2d3ba3db19036effaef3380c42438
Daniel Vrcic <daniel.vrcic@gmail.com>
2024-04-05 14:57:22 +0200

    initial custom Saml2Backend.authenticate() method for handling eduGAIN logins

--- hrzoosignup/backend/auth/saml2/backends.py
+++ hrzoosignup/backend/auth/saml2/backends.py
@@ -5,6 +5,29 @@ from backend.models import CrorisInstitutions
 
 
 class SAML2Backend(Saml2Backend):
+    def authenticate(self, request, session_info=None, attribute_mapping=None,
+                     create_unknown_user=True, assertion_info=None, **kwargs):
+        idp_entityid = session_info["issuer"]
+
+        if idp_entityid.startswith('https://edugainproxy.srce.hr/saml2/idp/'):
+            attributes = session_info['ava']
+            user_model = get_user_model()
+            first_name = attributes.get('givenName', None)
+            last_name = attributes.get('sn', None)
+            if isinstance(first_name, list):
+                first_name = first_name[0]
+            if isinstance(last_name, list):
+                last_name = last_name[0]
+            try:
+                user_found = user_model.objects.get(first_name=first_name, last_name=last_name)
+                if self.user_can_authenticate(user_found):
+                    return user_found
+            except user_model.DoesNotExist:
+                pass
+
+        else:
+            return super().authenticate(request, session_info, attribute_mapping, create_unknown_user, assertion_info, kwargs)
+
     def _update_user(self, user, attributes, attribute_mapping, force_save=False):
         try:
             if not user.person_institution_manual_set:
