42863ad8ba14d6a03184ceb556c085fa64d39112
Daniel Vrcic <daniel.vrcic@gmail.com>
2024-04-08 13:34:34 +0200

    align production SPConfig with tested devel environment

--- hrzoosignup/backend/auth/saml2/config.py
+++ hrzoosignup/backend/auth/saml2/config.py
@@ -14,36 +14,70 @@ def get_hostname(request):
 def get_saml_config(request):
     hostname = get_hostname(request)
 
-    config = {
-        'xmlsec_binary': '/usr/bin/xmlsec1',
-        'entityid': 'https://{}{}/saml2/metadata/'.format(hostname, settings.RELATIVE_PATH),
-        'allow_unknown_attributes': True,
-        'debug': 1,
-        'service': {
-            'sp': {
-                'name': 'HRZOO Signup',
-                'want_assertions_signed': True,
-                'force_authn': False,
-                'name_id_format_allow_create': False,
-                'want_response_signed': False,
-                'endpoints': {
-                    'assertion_consumer_service': [
-                        ('https://{}{}/saml2/acs/'.format(hostname, settings.RELATIVE_PATH),
-                         saml2.BINDING_HTTP_POST),
-                    ],
-                    'single_logout_service': [
-                        ('https://{}{}/saml2/ls/'.format(hostname, settings.RELATIVE_PATH),
-                         saml2.BINDING_HTTP_REDIRECT),
-                    ],
+    if 'edugain' in request.path:
+        config = {
+            'xmlsec_binary': '/usr/bin/xmlsec1',
+            'entityid': 'https://{}{}/saml2/edugain/metadata/'.format(hostname, settings.RELATIVE_PATH),
+            'allow_unknown_attributes': True,
+            'debug': 1,
+            'service': {
+                'sp': {
+                    'name': 'HRZOO Signup',
+                    'want_assertions_signed': True,
+                    'force_authn': False,
+                    'name_id_format_allow_create': False,
+                    'want_response_signed': False,
+                    'endpoints': {
+                        'assertion_consumer_service': [
+                            ('https://{}{}/saml2/edugain/acs/'.format(hostname, settings.RELATIVE_PATH),
+                             saml2.BINDING_HTTP_POST),
+                        ],
+                        'single_logout_service': [
+                            ('https://{}{}/saml2/edugain/ls/'.format(hostname, settings.RELATIVE_PATH),
+                             saml2.BINDING_HTTP_REDIRECT),
+                        ],
+                    },
+                    'attribute_map_dir': '{}/saml2/edugain/attributemaps/'.format(get_python_lib()),
                 },
-                'attribute_map_dir': '{}/saml2/attributemaps/'.format(get_python_lib()),
             },
-        },
-        'key_file': settings.HOST_KEY,  # private part
-        'cert_file': settings.HOST_CERT,  # public part
-        'metadata': {
-            'local': [settings.SAML_METADATA]
+            'key_file': settings.HOST_KEY,  # private part
+            'cert_file': settings.HOST_CERT,  # public part
+            'metadata': {
+                'local': [settings.SAML_METADATAEDUGAIN]
+            }
         }
-    }
+
+    else:
+        config = {
+            'xmlsec_binary': '/usr/bin/xmlsec1',
+            'entityid': 'https://{}{}/saml2/metadata/'.format(hostname, settings.RELATIVE_PATH),
+            'allow_unknown_attributes': True,
+            'debug': 1,
+            'service': {
+                'sp': {
+                    'name': 'HRZOO Signup',
+                    'want_assertions_signed': True,
+                    'force_authn': False,
+                    'name_id_format_allow_create': False,
+                    'want_response_signed': False,
+                    'endpoints': {
+                        'assertion_consumer_service': [
+                            ('https://{}{}/saml2/acs/'.format(hostname, settings.RELATIVE_PATH),
+                             saml2.BINDING_HTTP_POST),
+                        ],
+                        'single_logout_service': [
+                            ('https://{}{}/saml2/ls/'.format(hostname, settings.RELATIVE_PATH),
+                             saml2.BINDING_HTTP_REDIRECT),
+                        ],
+                    },
+                    'attribute_map_dir': '{}/saml2/attributemaps/'.format(get_python_lib()),
+                },
+            },
+            'key_file': settings.HOST_KEY,  # private part
+            'cert_file': settings.HOST_CERT,  # public part
+            'metadata': {
+                'local': [settings.SAML_METADATA]
+            }
+        }
 
     return SPConfig().load(config)
