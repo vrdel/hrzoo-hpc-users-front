6928aa41d82ac44410261cbe82b9867a8bd65567
Daniel Vrcic <daniel.vrcic@gmail.com>
2024-05-02 07:00:30 +0200

    invalidate cache if any project tag changed

--- hrzoosignup/backend/management/commands/jupyter_tag.py
+++ hrzoosignup/backend/management/commands/jupyter_tag.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
+from django.core.cache import cache
 
 
 class Command(BaseCommand):
@@ -20,11 +21,13 @@ class Command(BaseCommand):
         parser.add_argument(
             "--delete",
             action="store_true",
-            dest="del",
+            dest="delete",
             help="Remove JUPYTER tag",
         )
 
     def handle(self, *args, **options):
+        any_changed = False
+
         from backend import models
         if options['add']:
             for project in models.Project.objects.all():
@@ -39,6 +42,7 @@ class Command(BaseCommand):
                             })
                             self.stdout.write(self.style.SUCCESS(f"Added JUPYTER tag for {project.identifier}"))
                             project.save()
+                            any_changed = True
 
         elif options['delete']:
             for project in models.Project.objects.all():
@@ -47,7 +51,14 @@ class Command(BaseCommand):
                     if resources:
                         all_tags = [resource['label'] for resource in resources]
                         if 'JUPYTER' in all_tags:
-                            new_tags = [tag for tag in all_tags if tag['value'] != 'JUPYTER']
+                            new_tags = [tag for tag in resources if tag['value'] != 'JUPYTER']
                             project.staff_resources_type = new_tags
                             self.stdout.write(self.style.SUCCESS(f"Removed JUPYTER tag for {project.identifier}"))
                             project.save()
+                            any_changed = True
+
+        if any_changed:
+            cache.delete("usersinfoinactive-get")
+            cache.delete("usersinfo-get")
+            cache.delete("ext-users-projects")
+            cache.delete('projects-get-all')
