7ddf0f70f678a60da9d2e9bde0eed92d4b44607f
Daniel Vrcic <daniel.vrcic@gmail.com>
2024-05-02 06:55:23 +0200

    have add and delete arguments/feature for JUPYTER tag

--- hrzoosignup/backend/management/commands/add_jupyter_tag.py
+++ hrzoosignup/backend/management/commands/add_jupyter_tag.py
@@ -1,13 +1,6 @@
 from django.contrib.auth import get_user_model
-from django.core.management.base import BaseCommand, CommandError
-from django.conf import settings
-from django.db import DEFAULT_DB_ALIAS
-from django.db.utils import IntegrityError
-from django.utils import timezone
+from django.core.management.base import BaseCommand
 
-import json
-
-import datetime
 
 class Command(BaseCommand):
     help = 'Add JUPYTER tag in resource_type field if not exist'
@@ -15,9 +8,46 @@ class Command(BaseCommand):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.user_model = get_user_model()
 
-    def handle(self, *args, **kwargs):
+    def add_arguments(self, parser):
+        super(Command, self).add_arguments(parser)
+        parser.add_argument(
+            "--add",
+            action="store_true",
+            dest="add",
+            help="Add JUPYTER tag",
+        )
+        parser.add_argument(
+            "--delete",
+            action="store_true",
+            dest="del",
+            help="Remove JUPYTER tag",
+        )
+
+    def handle(self, *args, **options):
         from backend import models
-        for project in models.Project.objects.all():
-            pass
+        if options['add']:
+            for project in models.Project.objects.all():
+                if project.state.name in ['approve', 'extend', 'expire']:
+                    resources = project.staff_resources_type
+                    if resources:
+                        all_tags = [resource['label'] for resource in resources]
+                        if 'JUPYTER' not in all_tags:
+                            project.staff_resources_type.append({
+                                'label': 'JUPYTER',
+                                'value': 'JUPYTER'
+                            })
+                            self.stdout.write(self.style.SUCCESS(f"Added JUPYTER tag for {project.identifier}"))
+                            project.save()
+
+        elif options['delete']:
+            for project in models.Project.objects.all():
+                if project.state.name in ['approve', 'extend', 'expire']:
+                    resources = project.staff_resources_type
+                    if resources:
+                        all_tags = [resource['label'] for resource in resources]
+                        if 'JUPYTER' in all_tags:
+                            new_tags = [tag for tag in all_tags if tag['value'] != 'JUPYTER']
+                            project.staff_resources_type = new_tags
+                            self.stdout.write(self.style.SUCCESS(f"Removed JUPYTER tag for {project.identifier}"))
+                            project.save()
