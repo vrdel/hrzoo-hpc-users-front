12e4673a3fb9fd762f3ee66f975d5b8ee2f9c905
Daniel Vrcic <daniel.vrcic@gmail.com>
2024-05-10 13:08:34 +0200

    initial IntlProvider component import

--- hrzoosignup/frontend/src/main.jsx
+++ hrzoosignup/frontend/src/main.jsx
@@ -5,9 +5,11 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import BaseRoutes from './routes';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { IntlProvider } from 'react-intl';
 import 'Styles/bootstrap-custom.scss'
 
 const queryClient = new QueryClient()
+const defaultLocale = "hr-HR"
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
