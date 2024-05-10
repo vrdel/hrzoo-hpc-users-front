5336c71ef64ea44989caa46ffc2ea482bfdecad8
Daniel Vrcic <daniel.vrcic@gmail.com>
2024-05-11 00:07:41 +0200

    wrap in context so that we can programatically set locale and load translation messages

---
+++ hrzoosignup/frontend/src/components/IntlContextProvider.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import { IntlProvider } from 'react-intl';
+
+export const IntlContext = React.createContext({
+  locale: '',
+  setLocale: () => {}
+});
+const defaultLocale = 'en'
+
+
+export const IntlContextProvider = ( {children} ) => {
+  const [locale, setLocale] = useState(defaultLocale)
+  const [loadedMsgs, setLoadedMsgs] = useState(undefined)
+
+  function loadMsgs(locale) {
+    return import(`../compiled-lang/${locale}.json`).then(
+      (messages) => setLoadedMsgs(messages)
+    )
+  }
+
+  useEffect(() => {
+    loadMsgs(locale)
+  }, [locale])
+
+  if (locale && loadedMsgs) {
+    return (
+      <IntlContext.Provider value={{locale, setLocale}}>
+        <IntlProvider
+          defaultLocale={defaultLocale}
+          locale={locale}
+          messages={loadedMsgs}
+        >
+          {children}
+        </IntlProvider>
+      </IntlContext.Provider>
+    );
+  }
+  else
+    return null
+}
--- hrzoosignup/frontend/src/main.jsx
+++ hrzoosignup/frontend/src/main.jsx
@@ -5,36 +5,21 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import BaseRoutes from './routes';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { IntlProvider } from 'react-intl';
+import { IntlContextProvider } from 'Components/IntlContextProvider';
 import 'Styles/bootstrap-custom.scss'
 
 const queryClient = new QueryClient()
-const defaultLocale = "hr"
 
-
-function loadLocaleData(locale) {
-  switch (locale) {
-    case 'hr':
-      return import('./compiled-lang/hr.json')
-    //default:
-      //return import('compiled-lang/en.json')
-  }
-}
-
-const messages = await loadLocaleData(defaultLocale)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <IntlProvider
-      defaultLocale={defaultLocale}
-      messages={messages}
-    >
+    <IntlContextProvider>
       <HelmetProvider>
         <QueryClientProvider client={queryClient}>
           <ReactQueryDevtools initialIsOpen={false} />
           <BaseRoutes />
         </QueryClientProvider>
       </HelmetProvider>
-    </IntlProvider>
+    </IntlContextProvider>
   </React.StrictMode>
 )
