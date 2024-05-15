db5c963b2bb686c100744dced2ee53da464458e7
Daniel Vrcic <daniel.vrcic@gmail.com>
2024-05-15 11:28:18 +0200

    footer english link

--- hrzoosignup/frontend/src/components/Footer.jsx
+++ hrzoosignup/frontend/src/components/Footer.jsx
@@ -3,9 +3,12 @@ import SrceBigLogo from '../assets/pravisrce.png';
 import SrceLogoTiny from '../assets/srce-logo-e-mail-sig.png';
 import { FormattedMessage } from 'react-intl';
 import 'Styles/footer.css';
+import { useIntl } from 'react-intl'
 
 
 const Footer = () => {
+  const intl = useIntl()
+
   return (
     <div id="hzsi-footer" className="shadow-sm align-self-center border rounded pristupacnost">
       {
@@ -23,7 +26,14 @@ const Footer = () => {
       <div className="text-center pt-1 pb-2">
         <p>
           <small>Copyright © 2024{' '}
-            <a href="https://www.srce.unizg.hr/" target="_blank" style={{'textDecoration': 'none'}} rel="noopener noreferrer">
+            <a href={intl.formatMessage({
+                defaultMessage: "https://www.srce.unizg.hr/",
+                description: 'footer-link'
+              })}
+              target="_blank"
+              style={{'textDecoration': 'none'}}
+              rel="noopener noreferrer"
+            >
               <FormattedMessage
                 description="footer-srce"
                 defaultMessage="Sveučilišni računski centar (Srce)"
