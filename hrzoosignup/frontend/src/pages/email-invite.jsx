5491a4d2b55e5f3540aaf624b5f99fb1bc843cc7
Daniel Vrcic <daniel.vrcic@gmail.com>
2024-05-30 11:21:01 +0200

    translated card titles on EmailInvitation

--- hrzoosignup/frontend/src/pages/email-invite.jsx
+++ hrzoosignup/frontend/src/pages/email-invite.jsx
@@ -110,7 +110,20 @@ const EmailInvitation = ({sessionData=undefined}) => {
                   className="p-3 d-flex flex-row align-items-center justify-content-center"
                 >
                   <FontAwesomeIcon icon={faLaptopCode} style={{color: "#c00000"}} size="3x" />
-                  <h4 className="ms-4 text-dark"><strong>Napredno računanje</strong> - <span className="fst-italic">Pozivnica</span></h4>
+                  <h4 className="ms-4 text-dark">
+                    <strong>
+                      <FormattedMessage
+                        defaultMessage="Napredno računanje"
+                        description="email-invite-cardtitle-1"
+                      />
+                    </strong> - {' '}
+                    <span className="fst-italic">
+                      <FormattedMessage
+                        defaultMessage="Pozivnica"
+                        description="email-invite-cardtitle-2"
+                      />
+                    </span>
+                  </h4>
                 </CardHeader>
                 <CardBody className="pt-5">
                   <p className="fs-5 mb-4 text-center">
