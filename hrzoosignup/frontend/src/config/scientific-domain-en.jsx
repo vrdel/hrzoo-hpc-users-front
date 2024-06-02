dc935c47ee62448013f61c884b873adfcb50f1ba
Daniel Vrcic <daniel.vrcic@gmail.com>
2024-06-02 21:27:55 +0200

    add english translation of scientific domains and fields

---
+++ hrzoosignup/frontend/src/config/scientific-domain-en.jsx
@@ -0,0 +1,60 @@
+let naturalFields = [
+  "Mathematics", "Physics", "Geology", "Chemistry",
+  "Biology", "Geophysics", "Interdisciplinary Natural Sciences"
+]
+
+let techFields = [
+  "Architecture and Urbanism", "Naval Architecture",
+  "Electrical Engineering", "Geodesy", "Civil Engineering", "Graphic Technology",
+  "Chemical Engineering", "Metallurgy", "Computing", "Mining, Petroleum and Geological Engineering",
+  "Mechanical Engineering", "Traffic and Transport Technology", "Textile Technology",
+  "Aeronautics, Rocket and Space Technology", "Fundamental Technical Sciences",
+  "Interdisciplinary Technical Sciences"
+]
+
+let bioMedFields = [
+  "Fundamental Medical Sciences", "Clinical Medical Sciences",
+  "Public Health and Health Care",
+  "Veterinary Medicine", "Dental Medicine", "Pharmacy"
+]
+
+let bioTechFields = [
+  "Agriculture (Agronomy)", "Forestry", "Wood Technology",
+  "Biotechnology", "Food Technology", "Nutrition",
+  "Interdisciplinary Biotechnical Sciences"
+]
+
+let socialFields = [
+  "Economics", "Law", "Political Science",
+  "Information and Communication Sciences",
+  "Sociology", "Psychology", "Pedagogy",
+  "Educational-Rehabilitation Sciences",
+  "Speech Therapy", "Kinesiology", "Demography",
+  "Social Work", "Security and Defense Sciences",
+  "Interdisciplinary Social Sciences"
+]
+
+let humanitiesFields = [
+  "Philosophy", "Theology", "Philology", "History", "Art History",
+  "Science of Art", "Archaeology", "Ethnology and Anthropology",
+  "Religious Studies (Interdisciplinary Field)", "Interdisciplinary Humanities"
+]
+
+export const listScientificDomainEn = [
+  "NATURAL SCIENCES", "TECHNICAL SCIENCES", "BIOMEDICINE AND HEALTH",
+  "BIOTECHNICAL SCIENCES", "SOCIAL SCIENCES", "HUMANITIES",
+//  "ART FIELD", "INTERDISCIPLINARY FIELDS OF SCIENCE",
+//  "INTERDISCIPLINARY FIELDS OF ART"
+]
+
+export const mapDomainsToFieldsEn = {
+  "NATURAL SCIENCES": naturalFields,
+  "TECHNICAL SCIENCES": techFields,
+  "BIOMEDICINE AND HEALTH": bioMedFields,
+  "BIOTECHNICAL SCIENCES": bioTechFields,
+  "SOCIAL SCIENCES": socialFields,
+  "HUMANITIES": humanitiesFields,
+//  "ART FIELD": [],
+//  "INTERDISCIPLINARY FIELDS OF SCIENCE": [],
+//  "INTERDISCIPLINARY FIELDS OF ART": [],
+}
--- hrzoosignup/frontend/src/config/scientific-domain.jsx
+++ hrzoosignup/frontend/src/config/scientific-domain.jsx
@@ -11,16 +11,19 @@ let techFields = [
   "Zrakoplovstvo, raketna i svemirska tehnika", "Temeljne tehničke znanosti",
   "Interdisciplinarne tehničke znanosti"
 ]
+
 let bioMedFields = [
   "Temeljne medicinske znanosti", "Kliničke medicinske znanosti",
   "Javno zdravstvo i zdravstvena zaštita",
   "Veterinarska medicina", "Dentalna medicina", "Farmacija"
 ]
+
 let bioTechFields = [
   "Poljoprivreda (agronomija)", "Šumarstvo", "Drvna tehnologija",
   "Biotehnologija", "Prehrambena tehnologija", "Nutricionizam",
   "Interdisciplinarne biotehničke znanosti"
 ]
+
 let socialFields = [
   "Ekonomija", "Pravo", "Politologija",
   "Informacijske i komunikacijske znanosti",
@@ -30,12 +33,12 @@ let socialFields = [
   "Socijalne djelatnosti", "Sigurnosne i obrambene znanosti",
   "Interdisciplinarne društvene znanosti"
 ]
+
 let humanitiesFields = [
   "Filozofija", "Teologija", "Filologija", "Povijest", "Povijest umjetnosti",
   "Znanost o umjetnosti", "Arheologija", "Etnologija i antropologija",
   "Religijske znanosti (interdisciplinarno polje)", "Interdisciplinarne humanističke znanosti"
 ]
-
 
 export const listScientificDomain = [
   "PRIRODNE ZNANOSTI", "TEHNIČKE ZNANOSTI", "BIOMEDICINA I ZDRAVSTVO",
