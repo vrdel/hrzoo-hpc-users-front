6b8d2307ae3f69b2dec058dcc0a23550f370a872
Daniel Vrcic <daniel.vrcic@gmail.com>
2024-06-02 20:20:45 +0200

    translate project badges

--- hrzoosignup/frontend/src/config/map-projecttypes.jsx
+++ hrzoosignup/frontend/src/config/map-projecttypes.jsx
@@ -1,14 +1,37 @@
 import React from 'react';
+import { useIntl } from 'react-intl'
 
 
 export function TypeString(type_name) {
+  const intl = useIntl();
+
   let type2string = {
-    'research-croris': <>Istraživački<br/>projekt</>,
-    'research-institutional': <>Institucijski<br/>projekt</>,
-    'thesis': <>Izrada<br/>rada</>,
-    'practical': <>Praktična<br/>nastava</>,
-    'internal': <>Interni<br/>projekt</>,
-    'srce-workshop': <>Srce<br/>radionica</>,
+    'research-croris':
+    intl.formatMessage({
+      defaultMessage: "Istraživački{br}projekt",
+      description: "project-type-research",
+    },{br: <br/>}),
+    'research-institutional':
+    intl.formatMessage({
+      defaultMessage: "Institucijski{br}projekt",
+      description: "project-type-instit"
+    }, {br: <br/>}),
+    'thesis': intl.formatMessage({
+      defaultMessage: "Izrada{br}rada",
+      description: "project-type-thesis"
+    }, {br: <br/>}),
+    'practical': intl.formatMessage({
+      defaultMessage: "Praktična{br}nastava",
+      description: "project-type-practical"
+    }, {br: <br/>}),
+    'internal': intl.formatMessage({
+      defaultMessage: "Interni{br}projekt",
+      description: "project-type-internal"
+    }, {br: <br/>}),
+    'srce-workshop': intl.formatMessage({
+      defaultMessage: "Srce{br}radionica",
+      description: "project-type-workshop"
+    }, {br: <br/>}),
   }
 
   return type2string[type_name]
