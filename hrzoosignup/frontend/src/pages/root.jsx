174169f257e0290628c46d552f7f9a2a5b1617c1
Daniel Vrcic <daniel.vrcic@gmail.com>
2024-06-02 21:29:29 +0200

    place translation in root context

--- hrzoosignup/frontend/src/pages/root.jsx
+++ hrzoosignup/frontend/src/pages/root.jsx
@@ -4,6 +4,7 @@ import { LinkTitles } from 'Config/link-titles';
 import { RequestTypesToSelect, UrlToRequestType } from 'Config/request-types';
 import { ResourceTypesToSelect, ResourceTypesToSelectAdmin } from 'Config/resource-types';
 import { listScientificDomain, mapDomainsToFields } from 'Config/scientific-domain';
+import { listScientificDomainEn, mapDomainsToFieldsEn } from 'Config/scientific-domain-en';
 import { buildOptionsFromArray } from 'Utils/select-tools';
 import { AuthContextProvider } from 'Components/AuthContextProvider';
 
@@ -18,7 +19,9 @@ const Root = () => {
       ResourceTypesToSelect,
       ResourceTypesToSelectAdmin,
       listScientificDomain,
+      listScientificDomainEn,
       mapDomainsToFields,
+      mapDomainsToFieldsEn,
       buildOptionsFromArray,
     }}>
       <AuthContextProvider>
