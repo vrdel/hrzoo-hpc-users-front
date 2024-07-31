99cabffe807159462edeae79dd8478cdbe721dc5
Katarina Zailac <katarina.zailac@gmail.com>
2024-07-31 10:47:31 +0200

    Component to use when there are no accounting records

--- hrzoosignup/frontend/src/pages/accounting.jsx
+++ hrzoosignup/frontend/src/pages/accounting.jsx
@@ -14,7 +14,8 @@ const colors = ['#e8827a', '#b04c46','#d71635', '#510707', '#7e191e',  '#df7f1b'
 const MyAccounting = () => {
   const { userDetails } = useContext(AuthContext);
   const [padobranProjects, setPadobranProjects] = useState([])
-  const [supekProjects, setSupekProjects] = useState([])
+  const [supekCPUProjects, setSupekCPUProjects] = useState([])
+  const [supekGPUProjects, setSupekGPUProjects] = useState([])
 
   const { status, data, error } = useQuery({
     queryKey: ["graph-data", userDetails.username],
@@ -25,10 +26,11 @@ const MyAccounting = () => {
     if (status == "success" && data) {
       if ("supek" in data) {
         let supek_cpu = new Set(...data["supek"]["cpuh"].map(item => Object.keys(item)))
-        let supek_gpu = new Set(...data["supek"]["cpuh"].map(item => Object.keys(item)))
-        let supek = new Set([...supek_cpu, ...supek_gpu])
-        supek.delete("month")
-        setSupekProjects(Array.from(supek).sort())
+        let supek_gpu = new Set(...data["supek"]["gpuh"].map(item => Object.keys(item)))
+        supek_cpu.delete("month")
+        supek_gpu.delete("month")
+        setSupekCPUProjects(Array.from(supek_cpu).sort())
+        setSupekGPUProjects(Array.from(supek_gpu).sort())
       }
 
       if ("padobran" in data) {
@@ -59,53 +61,59 @@ const MyAccounting = () => {
           <PageTitle pageTitle="graphs" />
         </Row>
         {
-          supekProjects.length > 0 &&
+          (supekCPUProjects.length > 0 || supekGPUProjects.length > 0) &&
             <Row>
               <h3>Supek</h3> 
-              <Col md={6}>
-                <h4>CPUH</h4>
-                <BarChart
-                  width={ 600 }
-                  height={ 300 }
-                  data={ data["supek"]["cpuh"] }
-                  margin={{
-                    top: 5,
-                    right: 30,
-                    left: 20,
-                    bottom: 5
-                  }}
-                >
-                  <CartesianGrid strokeDasharray="3 3" />
-                  <XAxis dataKey="month" />
-                  <YAxis scale="log" domain={[1, "dataMax"]} />
-                  <Legend />
-                  {
-                    supekProjects.map((proj, index) => <Bar key={ proj } dataKey={ proj } fill={ colors[index] } />)
-                  }
-                </BarChart>
-              </Col>
-              <Col md={6}>
-                <h4>GPUH</h4>
-                <BarChart
-                  width={ 600 }
-                  height={ 300 }
-                  data={ data["supek"]["gpuh"] }
-                  margin={{
-                    top: 5,
-                    right: 30,
-                    left: 20,
-                    bottom: 5
-                  }}
-                >
-                  <CartesianGrid strokeDasharray="3 3" />
-                  <XAxis dataKey="month" />
-                  <YAxis scale="log" domain={[1, "dataMax"]} />
-                  <Legend />
-                  {
-                    supekProjects.map((proj, index) => <Bar key={ proj } dataKey={ proj } fill={ colors[index] } />)
-                  }
-                </BarChart>
-              </Col>
+              {
+                supekCPUProjects.length > 0 &&
+                  <Col md={6}>
+                    <h4>CPUH</h4>
+                    <BarChart
+                      width={ 600 }
+                      height={ 300 }
+                      data={ data["supek"]["cpuh"] }
+                      margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5
+                      }}
+                    >
+                      <CartesianGrid strokeDasharray="3 3" />
+                      <XAxis dataKey="month" />
+                      <YAxis />
+                      <Legend iconSize={10} />
+                      {
+                        supekCPUProjects.map((proj, index) => <Bar key={ proj } label={{ position: "top", fontSize: 10, fill: colors[index] }} dataKey={ proj } fill={ colors[index] } />)
+                      }
+                    </BarChart>
+                  </Col>
+              }
+              {
+                supekGPUProjects.length > 0 &&
+                  <Col md={6}>
+                    <h4>GPUH</h4>
+                    <BarChart
+                      width={ 600 }
+                      height={ 300 }
+                      data={ data["supek"]["gpuh"] }
+                      margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5
+                      }}
+                    >
+                      <CartesianGrid strokeDasharray="3 3" />
+                      <XAxis dataKey="month" />
+                      <YAxis />
+                      <Legend iconSize={10} />
+                      {
+                        supekGPUProjects.map((proj, index) => <Bar key={ proj } dataKey={ proj } label={{ position: "top", fill: colors[index], fontSize: 10 }} fill={ colors[index] } />)
+                      }
+                    </BarChart>
+                  </Col>
+              }
             </Row>
         }
         {
@@ -127,12 +135,20 @@ const MyAccounting = () => {
                 >
                   <CartesianGrid strokeDasharray="3 3" />
                   <XAxis dataKey="month" />
-                  <YAxis scale="log" domain={[1, "dataMax"]} />
-                  <Legend />
+                  <YAxis />
+                  <Legend iconSize={10} />
                   {
-                    padobranProjects.map((proj, index) => <Bar key={ proj } dataKey={ proj } fill={ colors[index] } />)
+                    padobranProjects.map((proj, index) => <Bar key={ proj } dataKey={ proj } label={{ position: "top", fill: colors[index], fontSize: 10 }} fill={ colors[index] } />)
                   }
                 </BarChart>
+              </Col>
+            </Row>
+        }
+        {
+          (supekCPUProjects.length == 0 && supekGPUProjects.length == 0 && padobranProjects == 0) &&
+            <Row className="mt-3 mb-3">
+              <Col className="d-flex align-items-center justify-content-center shadow-sm bg-light border border-danger rounded text-muted text-center p-3 fs-3" style={{height: '400px'}} md={{offset: 1, size: 10}}>
+                Nema zabilježenog iskorištenja resursa
               </Col>
             </Row>
         }
