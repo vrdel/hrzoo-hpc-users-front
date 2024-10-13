862f899bdbbe6e2cebc6dc146023a721847e6c38
Daniel Vrcic <daniel.vrcic@gmail.com>
2024-10-13 17:54:34 +0200

    use modern sass API to avoid warning

--- hrzoosignup/frontend/webpack.devserver.config.js
+++ hrzoosignup/frontend/webpack.devserver.config.js
@@ -43,6 +43,7 @@ module.exports = {
             loader: "sass-loader",
             options: {
               // Prefer `dart-sass`
+              api: "modern",
               implementation: require('sass'),
             },
           }
