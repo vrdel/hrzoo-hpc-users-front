cddb1813d7bedd100270ad9cf3cb1fe274f795ec
Katarina Zailac <katarina.zailac@gmail.com>
2024-11-25 11:42:21 +0100

    Return cumulative and monthly resource accounting in internal API

--- hrzoosignup/backend/api/tests/test_view_accounting.py
+++ hrzoosignup/backend/api/tests/test_view_accounting.py
@@ -26,67 +26,122 @@ class ResourceUsageTests(TestCase):
         response = self.view(request)
         self.assertEqual(
             response.data, {
-                "supek": {
-                    "cpuh": [
-                        {
-                            "month": "02/2024",
-                            "project-1": 1
-                        },
-                        {
-                            "month": "03/2024",
-                            "project-1": 1
-                        },
-                        {
-                            "month": "04/2024",
-                            "project-1": 1
-                        },
-                        {
-                            "month": "05/2024",
-                            "project-1": 1,
-                            "project-2": 117,
-                            "project-4": 1
-                        },
-                        {
-                            "month": "06/2024",
-                            "project-1": 2,
-                            "project-2": 117,
-                            "project-4": 1
-                        },
-                        {
-                            "month": "07/2024",
-                            "project-1": 2,
-                            "project-2": 117
-                        },
-                        {
-                            "month": "08/2024",
-                            "project-2": 117
-                        }
-                    ],
-                    "gpuh": [
-                        {
-                            "month": "02/2024"
-                        },
-                        {
-                            "month": "03/2024"
-                        },
-                        {
-                            "month": "04/2024"
-                        },
-                        {
-                            "month": "05/2024"
-                        },
-                        {
-                            "month": "06/2024",
-                            "project-1": 1
-                        },
-                        {
-                            "month": "07/2024",
-                            "project-1": 1
-                        },
-                        {
-                            "month": "08/2024"
-                        }
-                    ]
+                "cumulative": {
+                    "supek": {
+                        "cpuh": [
+                            {
+                                "month": "02/2024",
+                                "project-1": 1
+                            },
+                            {
+                                "month": "03/2024",
+                                "project-1": 1
+                            },
+                            {
+                                "month": "04/2024",
+                                "project-1": 1
+                            },
+                            {
+                                "month": "05/2024",
+                                "project-1": 1,
+                                "project-2": 117,
+                                "project-4": 1
+                            },
+                            {
+                                "month": "06/2024",
+                                "project-1": 2,
+                                "project-2": 117,
+                                "project-4": 1
+                            },
+                            {
+                                "month": "07/2024",
+                                "project-1": 2,
+                                "project-2": 117
+                            },
+                            {
+                                "month": "08/2024",
+                                "project-2": 117
+                            }
+                        ],
+                        "gpuh": [
+                            {
+                                "month": "02/2024"
+                            },
+                            {
+                                "month": "03/2024"
+                            },
+                            {
+                                "month": "04/2024"
+                            },
+                            {
+                                "month": "05/2024"
+                            },
+                            {
+                                "month": "06/2024",
+                                "project-1": 1
+                            },
+                            {
+                                "month": "07/2024",
+                                "project-1": 1
+                            },
+                            {
+                                "month": "08/2024"
+                            }
+                        ]
+                    }
+                },
+                "monthly": {
+                    "supek": {
+                        "cpuh": [
+                            {
+                                "month": "02/2024"
+                            },
+                            {
+                                "month": "03/2024"
+                            },
+                            {
+                                "month": "04/2024"
+                            },
+                            {
+                                "month": "05/2024",
+                                "project-2": 117,
+                                "project-4": 1
+                            },
+                            {
+                                "month": "06/2024"
+                            },
+                            {
+                                "month": "07/2024"
+                            },
+                            {
+                                "month": "08/2024"
+                            }
+                        ],
+                        "gpuh": [
+                            {
+                                "month": "02/2024"
+                            },
+                            {
+                                "month": "03/2024"
+                            },
+                            {
+                                "month": "04/2024"
+                            },
+                            {
+                                "month": "05/2024"
+                            },
+                            {
+                                "month": "06/2024",
+                                "project-1": 1
+                            },
+                            {
+                                "month": "07/2024"
+                            },
+                            {
+                                "month": "08/2024"
+                            }
+                        ]
+                    }
                 }
             }
         )
@@ -106,89 +161,166 @@ class ResourceUsageTests(TestCase):
     def test_get_data_if_resource_jupyter(self, mock_date_today):
         mock_date_today.return_value = datetime.date(2024, 8, 22)
         user = models.User.objects.get(person_uniqueid="user454@fer.hr")
-        request = self.factory.get(
-            "/api/v1/internal/accounting/records"
-        )
+        request = self.factory.get("/api/v1/internal/accounting/records")
         force_authenticate(request, user=user)
         response = self.view(request)
         self.assertEqual(
             response.data, {
-                "padobran": {
-                    "cpuh": [
-                        {
-                            "month": "02/2024",
-                        },
-                        {
-                            "month": "03/2024",
-                        },
-                        {
-                            "month": "04/2024",
-                        },
-                        {
-                            "month": "05/2024"
-                        },
-                        {
-                            "month": "06/2024"
-                        },
-                        {
-                            "month": "07/2024"
-                        },
-                        {
-                            "month": "08/2024"
-                        }
-                    ]
+                "cumulative": {
+                    "padobran": {
+                        "cpuh": [
+                            {
+                                "month": "02/2024",
+                            },
+                            {
+                                "month": "03/2024",
+                            },
+                            {
+                                "month": "04/2024",
+                            },
+                            {
+                                "month": "05/2024"
+                            },
+                            {
+                                "month": "06/2024"
+                            },
+                            {
+                                "month": "07/2024"
+                            },
+                            {
+                                "month": "08/2024"
+                            }
+                        ]
+                    },
+                    "jupyter": {
+                        "cpuh": [
+                            {
+                                "month": "02/2024",
+                            },
+                            {
+                                "month": "03/2024",
+                            },
+                            {
+                                "month": "04/2024",
+                            },
+                            {
+                                "month": "05/2024"
+                            },
+                            {
+                                "month": "06/2024"
+                            },
+                            {
+                                "month": "07/2024",
+                                "project-5": 5
+                            },
+                            {
+                                "month": "08/2024",
+                                "project-5": 5
+                            }
+                        ],
+                        "gpuh": [
+                            {
+                                "month": "02/2024",
+                            },
+                            {
+                                "month": "03/2024",
+                            },
+                            {
+                                "month": "04/2024",
+                            },
+                            {
+                                "month": "05/2024"
+                            },
+                            {
+                                "month": "06/2024"
+                            },
+                            {
+                                "month": "07/2024",
+                                "project-5": 3
+                            },
+                            {
+                                "month": "08/2024",
+                                "project-5": 3
+                            }
+                        ]
+                    }
                 },
-                "jupyter": {
-                    "cpuh": [
-                        {
-                            "month": "02/2024",
-                        },
-                        {
-                            "month": "03/2024",
-                        },
-                        {
-                            "month": "04/2024",
-                        },
-                        {
-                            "month": "05/2024"
-                        },
-                        {
-                            "month": "06/2024"
-                        },
-                        {
-                            "month": "07/2024",
-                            "project-5": 5
-                        },
-                        {
-                            "month": "08/2024",
-                            "project-5": 5
-                        }
-                    ],
-                    "gpuh": [
-                        {
-                            "month": "02/2024",
-                        },
-                        {
-                            "month": "03/2024",
-                        },
-                        {
-                            "month": "04/2024",
-                        },
-                        {
-                            "month": "05/2024"
-                        },
-                        {
-                            "month": "06/2024"
-                        },
-                        {
-                            "month": "07/2024",
-                            "project-5": 3
-                        },
-                        {
-                            "month": "08/2024",
-                            "project-5": 3
-                        }
-                    ]
+                "monthly": {
+                    "padobran": {
+                        "cpuh": [
+                            {
+                                "month": "02/2024",
+                            },
+                            {
+                                "month": "03/2024",
+                            },
+                            {
+                                "month": "04/2024",
+                            },
+                            {
+                                "month": "05/2024"
+                            },
+                            {
+                                "month": "06/2024"
+                            },
+                            {
+                                "month": "07/2024"
+                            },
+                            {
+                                "month": "08/2024"
+                            }
+                        ]
+                    },
+                    "jupyter": {
+                        "cpuh": [
+                            {
+                                "month": "02/2024",
+                            },
+                            {
+                                "month": "03/2024",
+                            },
+                            {
+                                "month": "04/2024",
+                            },
+                            {
+                                "month": "05/2024"
+                            },
+                            {
+                                "month": "06/2024"
+                            },
+                            {
+                                "month": "07/2024",
+                                "project-5": 5
+                            },
+                            {
+                                "month": "08/2024"
+                            }
+                        ],
+                        "gpuh": [
+                            {
+                                "month": "02/2024",
+                            },
+                            {
+                                "month": "03/2024",
+                            },
+                            {
+                                "month": "04/2024",
+                            },
+                            {
+                                "month": "05/2024"
+                            },
+                            {
+                                "month": "06/2024"
+                            },
+                            {
+                                "month": "07/2024",
+                                "project-5": 3
+                            },
+                            {
+                                "month": "08/2024"
+                            }
+                        ]
+                    }
                 }
             }
         )
