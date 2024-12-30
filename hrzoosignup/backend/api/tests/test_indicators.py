0c49bbba06cf45659534a586f938e6be9d733f54
Katarina Zailac <katarina.zailac@gmail.com>
2024-12-30 15:32:37 +0100

    Methods for calculation of other indicators

--- hrzoosignup/backend/api/tests/test_indicators.py
+++ hrzoosignup/backend/api/tests/test_indicators.py
@@ -1,5 +1,10 @@
+import datetime
+
+from backend import models
 from backend.dashboard.indicators import DashboardIndicators
 from django.test import TestCase
+from django.utils import timezone
+from django.utils.timezone import get_current_timezone
 
 from .test_utils import create_mock_db
 
@@ -7,6 +12,170 @@ from .test_utils import create_mock_db
 class DashboardTests(TestCase):
     def setUp(self):
         create_mock_db()
+        user1 = models.User.objects.get(username="user119@fer.hr")
+        user5 = models.User.objects.get(username="user42@fer.hr")
+        user7 = models.User.objects.get(username="delboy@pmf.hr")
+
+        project1 = models.Project.objects.get(identifier="project-1")
+        project3 = models.Project.objects.get(identifier="project-3")
+        project4 = models.Project.objects.get(identifier="project-4")
+
+        models.ResourceUsage.objects.create(
+            user=user1,
+            project=project1,
+            resource_name="supek",
+            end_time=timezone.make_aware(
+                datetime.datetime.fromtimestamp(1716376320),  # 22 May 2024
+                timezone=timezone.get_current_timezone()
+            ),
+            accounting_record={
+                "jobid": "7",
+                "walltime": 232141,
+                "start_time": 1716144179,
+                "queue": "queue1",
+                "wait_time": 2,
+                "qtime": 5,
+                "ngpu": 4,
+                "cpuh": 0,
+                "gpuh": 48
+            }
+        )
+        models.ResourceUsage.objects.create(
+            user=user5,
+            project=project3,
+            resource_name="supek",
+            end_time=timezone.make_aware(
+                datetime.datetime.fromtimestamp(1715685120),  # 14 May 2024
+                timezone=timezone.get_current_timezone()
+            ),
+            accounting_record={
+                "jobid": "7",
+                "walltime": 214135,
+                "start_time": 1715470985,
+                "queue": "queue1",
+                "wait_time": 17,
+                "qtime": 4321,
+                "ngpu": 4,
+                "cpuh": 0,
+                "gpuh": 128
+            }
+        )
+        models.ResourceUsage.objects.create(
+            user=user7,
+            project=project1,
+            resource_name="padobran",
+            end_time=timezone.make_aware(
+                datetime.datetime.fromtimestamp(1720520659),  # 9 Jul 2024
+                timezone=timezone.get_current_timezone()
+            ),
+            accounting_record={
+                "jobid": "12844",
+                "walltime": "23",
+                "ncpus": "2",
+                "start_time": "1720520646",
+                "queue": "queue2",
+                "wait_time": "4",
+                "qtime": "8",
+                "cpuh": 74,
+                "gpuh": 0
+            }
+        )
+        models.ResourceUsage.objects.create(
+            project=project4,
+            resource_name="cloud",
+            end_time=timezone.make_aware(
+                datetime.datetime.fromtimestamp(1716376320),  # 22 May 2024
+                timezone=timezone.get_current_timezone()
+            ),
+            accounting_record={
+                "start_time": 1716328801,
+                "instance_id": "122132132424312",
+                "vcpus": 16,
+                "started_at": 1716184372,
+                "ended_at": None,
+                "ngpus": 2,
+                "flavor": "m1.gpu.1",
+                "cpuh": 211.1956,
+                "gpuh": 26.3994
+            }
+        )
+        models.ResourceUsage.objects.create(
+            project=project1,
+            resource_name="cloud",
+            end_time=timezone.make_aware(
+                datetime.datetime.fromtimestamp(1715690970),  # 14 May 2024
+                timezone=timezone.get_current_timezone()
+            ),
+            accounting_record={
+                "start_time": 1715637601,
+                "instance_id": "d591480f-6e2b-4817-9c54-b12d0d2d731f",
+                "vcpus": 4,
+                "ngpus": 3,
+                "started_at": 1715599902,
+                "ended_at": None,
+                "flavor": "m1.half.windows",
+                "cpuh": 29.6494,
+                "gpuh": 44.4742
+            }
+        )
+        models.ResourceUsage.objects.create(
+            project=project1,
+            resource_name="cloud",
+            end_time=timezone.make_aware(
+                datetime.datetime.fromtimestamp(1715690970),  # 14 May 2024
+                timezone=timezone.get_current_timezone()
+            ),
+            accounting_record={
+                "start_time": 1715637601,
+                "instance_id": "13241243135132",
+                "vcpus": 64,
+                "ngpus": None,
+                "started_at": 1719313795,
+                "ended_at": 1727761972,
+                "flavor": "m1.medium",
+                "gpuh": 0,
+                "cpuh": 504.3733
+            }
+        )
+        models.ResourceUsage.objects.create(
+            project=project1,
+            user=user1,
+            resource_name="jupyter",
+            end_time=timezone.make_aware(
+                datetime.datetime(2024, 5, 17, 23, 48, 43),
+                timezone=get_current_timezone()
+            ),
+            accounting_record={
+                "jupyter_cpu_h": 17.17,
+                "jupyter_gpu_h": 0
+            }
+        )
+        models.ResourceUsage.objects.create(
+            project=project1,
+            user=user1,
+            resource_name="jupyter",
+            end_time=timezone.make_aware(
+                datetime.datetime(2024, 5, 14, 14, 23, 48),
+                timezone=get_current_timezone()
+            ),
+            accounting_record={
+                "jupyter_cpu_h": 0.73,
+                "jupyter_gpu_h": 7.18
+            }
+        )
+        models.ResourceUsage.objects.create(
+            project=project1,
+            user=user7,
+            resource_name="jupyter",
+            end_time=timezone.make_aware(
+                datetime.datetime(2024, 5, 14, 14, 23, 48),
+                timezone=get_current_timezone()
+            ),
+            accounting_record={
+                "jupyter_cpu_h": 13.73,
+                "jupyter_gpu_h": 8.23
+            }
+        )
         self.indicators = DashboardIndicators(month=5, year=2024)
 
     def test_institutions(self):
@@ -47,4 +216,54 @@ class DashboardTests(TestCase):
                 institution="Prirodoslovno-matematički fakultet, Zagreb"
             ),
             3
+        )
+
+    def test_supek_cpuh(self):
+        self.assertEqual(
+            self.indicators.supek_cpu(
+                institution="Fakultet elektrotehnike i računarstva"
+            ), 5
+        )
+
+    def test_supek_gpu(self):
+        self.assertEqual(
+            self.indicators.supek_gpu(
+                institution="Fakultet elektrotehnike i računarstva"
+            ), 176
+        )
+
+    def test_padobran(self):
+        indicators = DashboardIndicators(month=7, year=2024)
+        self.assertEqual(
+            indicators.padobran(
+                institution="Fakultet elektrotehnike i računarstva"
+            ), 74
+        )
+
+    def test_vrancic_cpu(self):
+        self.assertEqual(
+            self.indicators.vrancic_cpu(
+                institution="Fakultet elektrotehnike i računarstva"
+            ), 534
+        )
+
+    def test_vrancic_gpu(self):
+        self.assertEqual(
+            self.indicators.vrancic_gpu(
+                institution="Fakultet elektrotehnike i računarstva"
+            ), 44
+        )
+
+    def test_jupyter_cpu(self):
+        self.assertEqual(
+            self.indicators.jupyter_cpu(
+                institution="Fakultet elektrotehnike i računarstva"
+            ), 17
+        )
+
+    def test_jupyter_gpu(self):
+        self.assertEqual(
+            self.indicators.jupyter_gpu(
+                institution="Fakultet elektrotehnike i računarstva"
+            ), 7
         )
--- hrzoosignup/backend/dashboard/indicators.py
+++ hrzoosignup/backend/dashboard/indicators.py
@@ -3,11 +3,15 @@ import datetime
 
 import pytz
 from backend import models
+import math
 
 
 class DashboardIndicators:
     def __init__(self, month, year):
         self.start_date = datetime.date(year, month, 1)
+        self.start_datetime = datetime.datetime(
+            year, month, 1, 0, 0, 0, tzinfo=pytz.UTC
+        )
         self.end_date = datetime.datetime(
             year, month, calendar.monthrange(year, month)[1], 23, 59, 59,
             tzinfo=pytz.UTC
@@ -69,7 +73,7 @@ class DashboardIndicators:
     def projects(self, institution):
         return len(self._projects(institution=institution))
 
-    def users(self, institution):
+    def _users(self, institution):
         institution_users = [
             user.username for user in
             models.User.objects.filter(person_institution=institution)
@@ -89,4 +93,79 @@ class DashboardIndicators:
             ) > 0:
                 users.add(user)
 
-        return len(users)
+        return list(users)
+
+    def users(self, institution):
+        return len(self._users(institution))
+
+    def _supek_usage(self, institution):
+        return models.ResourceUsage.objects.filter(
+            project__in=self._projects(institution),
+            resource_name="supek"
+        )
+
+    def supek_cpu(self, institution):
+        return math.floor(sum(
+            float(item.accounting_record["cpuh"]) for item
+            in self._supek_usage(institution) if
+            self.start_datetime <= item.end_time <= self.end_date
+        ))
+
+    def supek_gpu(self, institution):
+        return math.floor(sum(
+            float(item.accounting_record["gpuh"]) for item
+            in self._supek_usage(institution) if
+            self.start_datetime <= item.end_time <= self.end_date
+        ))
+
+    def _vrancic_usage(self, institution):
+        return models.ResourceUsage.objects.filter(
+            project__in=self._projects(institution),
+            resource_name="cloud"
+        )
+
+    def vrancic_cpu(self, institution):
+        return math.floor(sum(
+            float(item.accounting_record["cpuh"]) for item in
+            self._vrancic_usage(institution) if
+            self.start_datetime <= item.end_time <= self.end_date
+        ))
+
+    def vrancic_gpu(self, institution):
+        return math.floor(sum(
+            float(item.accounting_record["gpuh"]) for item in
+            self._vrancic_usage(institution) if
+            self.start_datetime <= item.end_time <= self.end_date
+        ))
+
+
+    def padobran(self, institution):
+        usage = models.ResourceUsage.objects.filter(
+            project__in=self._projects(institution),
+            resource_name="padobran"
+        )
+
+        return math.floor(sum(
+            float(item.accounting_record["cpuh"]) for item in usage if
+            self.start_datetime <= item.end_time <= self.end_date
+        ))
+
+    def _jupyter_usage(self, institution):
+        return models.ResourceUsage.objects.filter(
+            user__username__in=self._users(institution),
+            resource_name="jupyter"
+        )
+
+    def jupyter_cpu(self, institution):
+        return math.floor(sum(
+            float(item.accounting_record["jupyter_cpu_h"]) for item in
+            self._jupyter_usage(institution) if
+            self.start_datetime <= item.end_time <= self.end_date
+        ))
+
+    def jupyter_gpu(self, institution):
+        return math.floor(sum(
+            float(item.accounting_record["jupyter_gpu_h"]) for item in
+            self._jupyter_usage(institution) if
+            self.start_datetime <= item.end_time <= self.end_date
+        ))
