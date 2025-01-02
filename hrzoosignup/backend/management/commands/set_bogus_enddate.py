f2b58cadf63960e6d4980b3ee9b7502ee396bf10
Daniel Vrcic <daniel.vrcic@gmail.com>
2025-01-02 16:53:37 +0100

    initial set of bogus_end date and refresh for each new day

--- hrzoosignup/backend/management/commands/set_bogus_enddate.py
+++ hrzoosignup/backend/management/commands/set_bogus_enddate.py
@@ -24,13 +24,30 @@ class Command(BaseCommand):
             help="Make changes",
         )
 
-    def handle(self, *args, **kwargs):
+    def handle(self, *args, **options):
         any_changed = False
         from backend import models
+        outdated = 0
+        refresh = 0
 
         for project in models.Project.objects.all():
             if project.state.name in ["expire", "submit", "deny"]:
                 continue
             date_now = timezone.make_aware(datetime.datetime.now()).date()
             if project.date_end < date_now:
-                self.stdout.write(self.style.NOTICE('Outdated active project {} with date_end {}'.format(project.identifier, project.date_end)))
+                outdated += 1
+                if not project.bogus_end:
+                    self.stdout.write(self.style.WARNING('Outdated active project {} with date_end {} without bogus_end set'.format(project.identifier, project.date_end)))
+                if options.get('confirm_yes', None) and not project.bogus_end:
+                    self.stdout.write(self.style.NOTICE(f'Set bogus_enddate={date_now} for project {project.identifier}'))
+                    any_changed = True
+                    project.bogus_end = date_now
+                if options.get('confirm_yes', None) and project.bogus_end and project.bogus_end != date_now:
+                    project.bogus_end = date_now
+                    any_changed = True
+                    refresh += 1
+                if any_changed:
+                    project.save()
+        self.stdout.write(f'Found {outdated} outdated but still active projects')
+        if refresh:
+            self.stdout.write(f'Refreshed bogus_end field for {refresh} outdated but still active projects')
