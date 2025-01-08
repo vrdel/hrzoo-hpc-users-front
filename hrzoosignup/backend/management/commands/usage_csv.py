eba44c3640583382d41004cbac357d651426d0c9
Katarina Zailac <katarina.zailac@gmail.com>
2025-01-08 10:02:33 +0100

    Command that dumps yearly accounting data to a .csv file

---
+++ hrzoosignup/backend/management/commands/usage_csv.py
@@ -0,0 +1,143 @@
+import datetime
+import time
+
+import pandas as pd
+from backend import models
+from django.core.management.base import BaseCommand
+from django.db.models import Q
+from django.utils import timezone
+from memory_profiler import profile
+
+
+def get_field(item, field):
+    try:
+        return item["accounting_record"][field]
+
+    except KeyError:
+        return None
+
+
+def get_wait_time(item):
+    try:
+        return get_field(item, "wait_time")
+
+    except TypeError:
+        return None
+
+
+def job_tag(item):
+    if item.resource_name in ["supek", "padobran"]:
+        if "gpu" in item["accounting_record"]["queue"]:
+            return "GPU"
+
+        else:
+            return "CPU"
+
+    else:
+        return None
+
+
+def get_realm(institutions, institution):
+    try:
+        return institutions[institution]
+
+    except KeyError:
+        return ""
+
+
+def get_finance(item):
+    try:
+        return item["project__croris_finance"][0]
+
+    except TypeError:
+        return ""
+
+
+def get_instance_id(item):
+    try:
+        return get_field(item, "instance_id")
+
+    except KeyError:
+        return ""
+
+
+class Command(BaseCommand):
+    help = "Export usage data to .csv file"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "-f", "--file", type=str, dest="filename", required=True,
+            help="full path to the .csv file"
+        )
+        parser.add_argument(
+            "-y", "--year", type=int, dest="year",
+            help="year for which you want information generated"
+        )
+
+    @profile
+    def handle(self, *args, **options):
+        start_time = time.time()
+        year = options["year"]
+
+        start_datetime = timezone.make_aware(
+            datetime.datetime(year, 1, 1, 0, 0, 0),
+            timezone=timezone.get_current_timezone()
+        )
+        start_date =  datetime.date(year, 1, 1)
+
+        usage = models.ResourceUsage.objects.filter(
+            Q(end_time__gte=start_datetime) &
+            ~Q(resource_name="jupyter") & (
+                Q(project__date_end__gte=start_date) |
+                Q(project__bogus_end__gte=start_date)
+            )
+        )
+
+        institutions = dict()
+        for item in models.CrorisInstitutions.objects.all():
+            institutions.update({item.name_long: item.realm})
+
+        data = pd.DataFrame.from_records(
+            usage.values(
+                "project__name",
+                "project__institute",
+                "project__project_type__name",
+                "project__croris_finance",
+                "resource_name",
+                "accounting_record"
+            )
+        )
+
+        # data = data[
+        #     (data["project__date_end"] >= start_date) |
+        #     (data["project__bogus_end"] >= start_date)
+        # ]
+
+        data["cpuh"] = data.apply(lambda row: get_field(row, "cpuh"), axis=1)
+        data["gpuh"] = data.apply(lambda row: get_field(row, "gpuh"), axis=1)
+        data["walltime"] = data.apply(
+            lambda row: get_field(row, "walltime"), axis=1
+        )
+        data["wait_time"] = data.apply(lambda row: get_wait_time(row), axis=1)
+        data["realm"] = data.apply(
+            lambda row: get_realm(institutions, row["project__institute"]),
+            axis=1
+        )
+        data["tag"] = data.apply(lambda row: job_tag(row), axis=1)
+        data["finance"] = data.apply(lambda row: get_finance(row), axis=1)
+        data["VM"] = data.apply(lambda row: get_instance_id(row), axis=1)
+
+        data.drop([
+            "project__croris_finance",
+            "accounting_record"
+        ], axis="columns", inplace=True)
+
+        data = data.rename(columns={
+            "project__name": "project",
+            "project__institute": "institution",
+            "project__project_type__name": "type"
+        })
+
+        data.to_csv(options["filename"], index=False, sep="*")
+
+        print(f"Time of execution: {time.time() - start_time} seconds")
