d7cb02264e09ac50c7c57bd2ff28dac49ff4b280
Daniel Vrcic <daniel.vrcic@gmail.com>
2025-01-19 08:55:06 +0100

    reflect changes of croris_finance in ProjectsListForm and ManageRequestsTable components

--- hrzoosignup/frontend/src/pages/projects/list.jsx
+++ hrzoosignup/frontend/src/pages/projects/list.jsx
@@ -130,7 +130,7 @@ const ProjectsListForm = ({ data, pageTitle }) => {
 
     else if (searchType === 'research-eu-croris')
       fieldsView = fieldsView.filter(e => e.project_type.name === 'research-croris' &&
-        _.findIndex(e.croris_finance, (fin) => fin.toLowerCase().includes('euro')) > -1)
+        _.findIndex(e.croris_finance, (fin) => fin.name?.toLowerCase().includes('euro')) > -1)
 
     else if (searchType.toLowerCase() === "all")
       fieldsView = fieldsView.filter(e => allProjectTypes.includes(e.project_type.name.toLowerCase()))
@@ -409,7 +409,7 @@ const ProjectsListForm = ({ data, pageTitle }) => {
                         <span className={ `badge fw-normal position-relative ${TypeColor(project.project_type.name)}` }>
                           { TypeString(project.project_type.name) }
                           {
-                            _.findIndex(project.croris_finance, (fin) => fin.toLowerCase().includes('euro')) > -1 &&
+                            _.findIndex(project.croris_finance, (fin) => fin.name.toLowerCase().includes('euro')) > -1 &&
                             <span className="position-absolute fw-normal top-100 start-100 translate-middle badge rounded-pill bg-danger">
                               EU
                               <span className="visually-hidden">EU</span>
