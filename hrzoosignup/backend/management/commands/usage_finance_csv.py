997c4815b617d97168c629e4b4dfef72b86ba285
Katarina Zailac <katarina.zailac@gmail.com>
2025-01-21 13:36:53 +0100

    Command to store finance data to .csv

---
+++ hrzoosignup/backend/management/commands/usage_finance_csv.py
@@ -0,0 +1,60 @@
+import datetime
+
+import pandas as pd
+from django.core.management.base import BaseCommand
+
+from .usage_project_csv import get_active_projects
+
+
+class Command(BaseCommand):
+    help = "Export project financing to .csv file"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "-f", "--file", type=str, dest="filename",
+            help="full path to the .csv file"
+        )
+        parser.add_argument(
+            "-y", "--year", type=int, dest="year",
+            help="year for which you want information generated"
+        )
+
+    def handle(self, *args, **options):
+        year = options["year"]
+
+        croris_projects = [
+            project for project in get_active_projects(
+                start_date=datetime.datetime(year, 1, 1, 0, 0, 0),
+                end_date=datetime.datetime(year, 12, 31, 23, 59, 59)
+            ) if project.project_type.name == "research-croris"
+        ]
+
+        sources = list()
+        projects = list()
+        percentages = list()
+        amounts = list()
+        currencies = list()
+        for project in croris_projects:
+            total_amount = sum(
+                [item["amount"] for item in project.croris_finance]
+            )
+            for finance in project.croris_finance:
+                sources.append(finance["name"])
+                if total_amount == 0:
+                    percentages.append(0)
+                else:
+                    percentages.append(finance["amount"] / total_amount * 100)
+
+                amounts.append(finance["amount"])
+                currencies.append(finance["currency"])
+                projects.append(project.name)
+
+        data = pd.DataFrame({
+            "source": sources,
+            "project": projects,
+            "percentage": percentages,
+            "amount": amounts,
+            "currency": currencies
+        })
+
+        data.to_csv(options["filename"], index=False, sep="*")
