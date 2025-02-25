d13a35431ccfab7dad7a9467229d5338938335a7
Katarina Zailac <katarina.zailac@gmail.com>
2025-02-25 10:59:28 +0100

    New API view for project leaders with info on projects

--- hrzoosignup/backend/api/internal/view_accounting.py
+++ hrzoosignup/backend/api/internal/view_accounting.py
@@ -136,32 +136,6 @@ def _generate_usage(df, dates, iterable=None, per_user=False):
             if "month" not in gpu_monthly:
                 gpu_monthly.update({"month": f"{month:02d}/{year}"})
 
-            if resource == "cloud":
-                _update_dict(
-                    resource_dict=cpu_cumulative,
-                    df_in=df_cumulative,
-                    value="cpuh",
-                    key="total_project_usage"
-                )
-                _update_dict(
-                    resource_dict=gpu_cumulative,
-                    df_in=df_cumulative,
-                    value="gpuh",
-                    key="total_project_usage"
-                )
-                _update_dict(
-                    resource_dict=cpu_monthly,
-                    df_in=df_monthly,
-                    value="cpuh",
-                    key="total_project_usage"
-                )
-                _update_dict(
-                    resource_dict=gpu_monthly,
-                    df_in=df_monthly,
-                    value="gpuh",
-                    key="total_project_usage"
-                )
-
             for item in iterable:
                 cpuh_key = "cpuh"
                 gpuh_key = "gpuh"
@@ -187,30 +161,31 @@ def _generate_usage(df, dates, iterable=None, per_user=False):
                         cpuh_key = "jupyter_cpuh"
                         gpuh_key = "jupyter_gpuh"
 
-                _update_dict(
-                    resource_dict=cpu_cumulative,
-                    df_in=df_cumulative_per_item,
-                    value=cpuh_key,
-                    key=item_key
-                )
-                _update_dict(
-                    resource_dict=gpu_cumulative,
-                    df_in=df_cumulative_per_item,
-                    value=gpuh_key,
-                    key=item_key
-                )
-                _update_dict(
-                    resource_dict=cpu_monthly,
-                    df_in=df_monthly_per_item,
-                    value=cpuh_key,
-                    key=item_key
-                )
-                _update_dict(
-                    resource_dict=gpu_monthly,
-                    df_in=df_monthly_per_item,
-                    value=gpuh_key,
-                    key=item_key
-                )
+                if not (resource == "cloud" and per_user):
+                    _update_dict(
+                        resource_dict=cpu_cumulative,
+                        df_in=df_cumulative_per_item,
+                        value=cpuh_key,
+                        key=item_key
+                    )
+                    _update_dict(
+                        resource_dict=gpu_cumulative,
+                        df_in=df_cumulative_per_item,
+                        value=gpuh_key,
+                        key=item_key
+                    )
+                    _update_dict(
+                        resource_dict=cpu_monthly,
+                        df_in=df_monthly_per_item,
+                        value=cpuh_key,
+                        key=item_key
+                    )
+                    _update_dict(
+                        resource_dict=gpu_monthly,
+                        df_in=df_monthly_per_item,
+                        value=gpuh_key,
+                        key=item_key
+                    )
 
             if cpu_cumulative:
                 cpuh_cumulative.append(cpu_cumulative)
@@ -274,11 +249,7 @@ def _generate_usage(df, dates, iterable=None, per_user=False):
     return output
 
 
-def usage4user(username):
-    records = models.ResourceUsage.objects.filter(
-        user=models.User.objects.get(person_username=username)
-    )
-
+def _project_info(records):
     output = dict()
     if len(records) > 0:
         df = pd.DataFrame.from_records(
@@ -311,7 +282,15 @@ def usage4user(username):
     return output
 
 
-def usage4project(lead_username):
+def usage4user(username):
+    records = models.ResourceUsage.objects.filter(
+        user=models.User.objects.get(person_username=username)
+    )
+
+    return _project_info(records)
+
+
+def _leader_records(lead_username):
     projects = [
         item.project for item in models.UserProject.objects.filter(
             user=models.User.objects.get(person_username=lead_username),
@@ -322,6 +301,12 @@ def usage4project(lead_username):
     records = models.ResourceUsage.objects.filter(
         Q(project__in=projects) & ~Q(resource_name="jupyter")
     )
+
+    return projects, records
+
+
+def usage4project_per_user(lead_username):
+    projects, records = _leader_records(lead_username)
 
     output = dict()
     if len(records) > 0:
@@ -372,6 +357,12 @@ def usage4project(lead_username):
     return output
 
 
+def usage4project(lead_username):
+    records = _leader_records(lead_username)[1]
+
+    return _project_info(records)
+
+
 class ResourceUsage(APIView):
     authentication_classes = (SessionAuthentication,)
     permission_classes = (IsAuthenticated,)
@@ -388,6 +379,31 @@ class ResourceUsage(APIView):
             output = usage4user(user.person_username)
 
             return Response(data=output, status=status.HTTP_200_OK)
+
+
+class ProjectUsage(APIView):
+    authentication_classes = (SessionAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        user = request.user
+
+        if not _is_user_lead(user):
+            err_status = status.HTTP_401_UNAUTHORIZED
+            err_response = {
+                "status": {
+                    "code": err_status,
+                    "message": "Only project leaders are allowed this view"
+                }
+            }
+
+            return Response(err_response, status=err_status)
+
+        else:
+            return Response(
+                data=usage4project(user.person_username),
+                status=status.HTTP_200_OK
+            )
 
 
 class ProjectUsagePerUser(APIView):
@@ -409,13 +425,15 @@ class ProjectUsagePerUser(APIView):
             return Response(err_response, status=err_status)
 
         else:
-            cached_data = cache.get(f"project_usage_{user.person_username}")
+            cached_data = cache.get(
+                f"project_user_usage_{user.person_username}"
+            )
 
             if cached_data:
                 return Response(data=cached_data, status=status.HTTP_200_OK)
 
             else:
                 return Response(
-                    usage4project(user.person_username),
+                    usage4project_per_user(user.person_username),
                     status=status.HTTP_200_OK
                 )
