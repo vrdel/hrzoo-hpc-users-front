ae3dd95a7f08cf53ca32a89bb0b265794e5f9b70
Katarina Zailac <katarina.zailac@gmail.com>
2025-02-25 11:40:59 +0100

    New function to fetch project usage data

--- hrzoosignup/frontend/src/api/accounting.jsx
+++ hrzoosignup/frontend/src/api/accounting.jsx
@@ -60,3 +60,33 @@ export async function fetchProjectUserAccountingData()
   if (error_msg)
     throw new Error(`Error fetching project accounting data: ${error_msg}`)
 }
+
+
+export async function fetchProjectAccountingData()
+{
+  let error_msg = ''
+
+  try {
+    let response = await fetch(`${url_api_prefix}/api/v1/internal/accounting/project-records`)
+
+    if (response.ok)
+      return await response.json()
+
+    if (!response.ok) {
+      try {
+        await response.json();
+        error_msg = `${response.status} ${response.statusText} in GET`
+      }
+      catch (err1) {
+        error_msg = `${response.status} ${response.statusText} in GET`
+      }
+    }
+
+  }
+  catch (err) {
+    error_msg = `${err} in GET`;
+  }
+
+  if (error_msg)
+    throw new Error(`Error fetching project accounting data: ${error_msg}`)
+}
