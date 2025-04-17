aa6a337286fca487d0a5848157d188eede6b3cdb
Katarina Zailac <katarina.zailac@gmail.com>
2025-04-17 09:34:14 +0200

    Show only swagger documentation for accounting API endpoints

--- hrzoosignup/hrzoosignup/excluded_path.py
+++ hrzoosignup/hrzoosignup/excluded_path.py
@@ -1,11 +1,6 @@
 def custom_preprocessing_hook(endpoints):
    filtered = []
    for (path, path_regex, method, callback) in endpoints:
-       if (
-               not path.startswith("/api/v1/internal/") and
-               not path.startswith("/auth") and
-               not path.startswith("/api/v1/sessionactive") and
-               not path.startswith("/api/v1/sshkeys")
-       ):
+       if path.startswith("/api/v1/accounting"):
            filtered.append((path, path_regex, method, callback))
    return filtered
