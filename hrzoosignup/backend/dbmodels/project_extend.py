7b9cb1ffb1b1a73cb146b5f5482512738c037396
Daniel Vrcic <daniel.vrcic@gmail.com>
2025-05-17 12:47:57 +0200

    approval track fields and changed date_end to DateField to match Project model

--- hrzoosignup/backend/dbmodels/project_extend.py
+++ hrzoosignup/backend/dbmodels/project_extend.py
@@ -6,12 +6,12 @@ from .project import Project
 
 class ProjectExtend(models.Model):
     date = models.DateTimeField(
-        _("Datetime when comment is added"),
+        _("Datetime when extension request is submitted"),
         null=True,
         blank=True
     )
     approved = models.BooleanField()
-    date_end = models.DateTimeField(
+    date_end = models.DateField(
         _("New date_end of project"),
         null=True,
         blank=True
@@ -19,5 +19,14 @@ class ProjectExtend(models.Model):
     reason = models.CharField(
         _('reason'),
         max_length=4096,
+    )
+    date_approved = models.DateTimeField(
+        null=True,
+        blank=True,
+    )
+    approved_by = models.JSONField(
+        _("JSONField with few details of staff that approved request"),
+        blank=True,
+        null=True
     )
     project = models.ForeignKey(Project, null=True, on_delete=models.CASCADE)
