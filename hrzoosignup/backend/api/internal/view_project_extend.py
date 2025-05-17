9c19f5870b108fa77f106daaa98a9a3507343727
Daniel Vrcic <daniel.vrcic@gmail.com>
2025-05-17 09:43:25 +0200

    POST allowed for project lead only and if project in approve-expire state

--- hrzoosignup/backend/api/internal/view_project_extend.py
+++ hrzoosignup/backend/api/internal/view_project_extend.py
@@ -1,25 +1,11 @@
 from rest_framework import status
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-# TODO: dev only
-from rest_framework.permissions import AllowAny
-
-from django.conf import settings
-from django.core.cache import cache
-from django.utils import timezone
-
 from backend import models
-from backend.email import project
-from backend.serializers import ProjectSerializer, ProjectSerializerGet, UserProjectSerializer
-from backend.utils.gen_username import gen_username
 
-import json
-import datetime
-import textwrap
 import logging
 
 logger = logging.getLogger('hrzoosignup.views')
@@ -31,7 +17,29 @@ class ProjectExtend(APIView):
 
     def post(self, request, **kwargs):
         projid = kwargs.get('projid')
-        pass
+
+        try:
+            p_obj = models.UserProject.objects.get(project__identifier=projid, user=request.user, role__name='lead')
+
+            if p_obj.project.state.name != 'approve-expire':
+                msg = {
+                    'status': {
+                        'code': status.HTTP_400_BAD_REQUEST,
+                        'message': '{} - Project is not in approve-expire state'.format(request.user.username)
+                    }
+                }
+                logger.error(msg)
+                return Response(msg, status=status.HTTP_400_BAD_REQUEST)
+
+        except models.UserProject.DoesNotExist:
+            err_response = {
+                'status': {
+                    'code': status.HTTP_404_NOT_FOUND,
+                    'message': '{} - UserProject not found'.format(request.user.username)
+                }
+            }
+            logger.error(err_response)
+            return Response(err_response, status=status.HTTP_404_NOT_FOUND)
 
     def get(self, request, **kwargs):
         pass
