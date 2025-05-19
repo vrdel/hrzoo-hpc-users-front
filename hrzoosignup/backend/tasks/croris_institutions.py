7783591a37cc0c03dcf072782457f66a635b04b3
Daniel Vrcic <daniel.vrcic@gmail.com>
2025-05-19 13:30:45 +0200

    remove authn on institutions sync API methods

--- hrzoosignup/backend/tasks/croris_institutions.py
+++ hrzoosignup/backend/tasks/croris_institutions.py
@@ -15,9 +15,7 @@ logger = logging.getLogger('hrzoosignup.tasks')
 class FetchCrorisInstitution(object):
     def __init__(self):
         # self.headers = {'Accept': 'application/json'}
-        auth = (settings.CRORIS_USER, settings.CRORIS_PASSWORD)
-        self.session = SessionWithRetry(logger, auth=auth,
-                                        handle_session_close=True)
+        self.session = SessionWithRetry(logger, handle_session_close=True)
 
     async def run(self):
         try:
