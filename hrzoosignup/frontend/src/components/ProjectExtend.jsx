31504642cc2aabc028a7e16fbe154ea96d09d341
Daniel Vrcic <daniel.vrcic@gmail.com>
2025-05-19 15:10:55 +0200

    initial modal component ProjectExtend

---
+++ hrzoosignup/frontend/src/components/ProjectExtend.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalFooter,
+  ModalHeader,
+}
+from 'reactstrap';
+import {FormattedMessage} from 'react-intl';
+
+
+export const ProjectExtend = ({isOpen, toggle, title, msg, onYes}) => (
+  <Modal isOpen={isOpen} toggle={toggle}>
+    <ModalHeader toggle={toggle}>{title}</ModalHeader>
+    <ModalBody>
+      {msg}
+    </ModalBody>
+    <ModalFooter>
+      <Button color="primary" onClick={() => {
+        onYes();
+        toggle();
+      }}>
+        <FormattedMessage defaultMessage="Da" description="modal-buttonyes" />
+      </Button>{' '}
+      <Button color="secondary" onClick={toggle}>
+        <FormattedMessage defaultMessage="Ne" description="modal-buttonno" />
+      </Button>
+    </ModalFooter>
+  </Modal>
+)
+
+export default ProjectExtend
