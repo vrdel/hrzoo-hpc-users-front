0085ff209b84a6a48fa7dc9c050eda01d1a529e0
Daniel Vrcic <daniel.vrcic@gmail.com>
2025-05-25 09:47:21 +0200

    allow invitation accept of foreigner on practical

--- hrzoosignup/backend/api/internal/view_invite.py
+++ hrzoosignup/backend/api/internal/view_invite.py
@@ -139,7 +139,8 @@ class Invites(APIView):
 
                 # foreign collaborators must authn via eduGAIN
                 # if proj_type.name == 'research-croris' and inv_type == 'foreign' and not edugain_authn:
-                if proj_type.name == 'research-croris' and inv_type == 'foreign' and request.user.person_type == 'local':
+                if (proj_type.name == 'research-croris' or proj_type.name == 'practical') \
+                        and inv_type == 'foreign' and request.user.person_type == 'local':
                     msg = {
                         'status': {
                             'code': status.HTTP_400_BAD_REQUEST,
@@ -175,7 +176,7 @@ class Invites(APIView):
                     return Response(msg, status=status.HTTP_400_BAD_REQUEST)
 
                 # (inv_type == 'foreign' and edugain_authn)):
-                if (proj_type.name == 'research-croris'):
+                if (proj_type.name == 'research-croris' or proj_type.name == 'practical'):
                     if ((inv_oib == request.user.person_oib and inv_type == 'local') or
                         (inv_type == 'foreign' and request.user.person_type == 'foreign')):
                         associate_user_to_project(user, proj)
