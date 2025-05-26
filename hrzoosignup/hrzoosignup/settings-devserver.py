9faf3410798c3f20bc0fc0ce8b788091dd1e7b76
Daniel Vrcic <daniel.vrcic@gmail.com>
2025-05-26 21:26:38 +0200

    introduce new config options ExtendWarningDays

--- hrzoosignup/hrzoosignup/settings-devserver.py
+++ hrzoosignup/hrzoosignup/settings-devserver.py
@@ -38,6 +38,7 @@ try:
     REALM_MAP = config.get('GENERAL', 'RealmMap')
     INSTITUTION_MAP = config.get('GENERAL', 'InstitutionMap')
     PROJECT_IDENTIFIER_MAP = config.get('GENERAL', 'IdentifierMap')
+    EXTEND_WARNING_DAYS = config.get('GENERAL', 'ExtendWarningDays')
 
     ALLOWED_HOSTS = config.get('SECURITY', 'AllowedHosts')
     HOST_CERT = config.get('SECURITY', 'HostCert')
