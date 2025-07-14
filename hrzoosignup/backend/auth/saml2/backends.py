4b593ab4c73c21d918d887747fcc4bad6d827cd4
Daniel Vrcic <daniel.vrcic@gmail.com>
2025-07-14 21:50:35 +0200

    generic attrs set check with complete missing info

--- hrzoosignup/backend/auth/saml2/backends.py
+++ hrzoosignup/backend/auth/saml2/backends.py
@@ -51,11 +51,16 @@ class SAML2Backend(Saml2Backend):
             institute = attributes.get('o', '')
             affiliation = attributes.get('eduPersonAffiliation', '')
 
+            present_attrs = set()
             for attr in settings.SAML_EDUGAINATTRS:
-                if not attributes.get(attr):
-                    logger.error(f'Mandatory attribute {attr} not presented')
-                    request.saml2_edugainattrs = True
-                    return None
+                if attributes.get(attr, None):
+                    present_attrs.add(attr)
+
+            missing = set(settings.SAML_EDUGAINATTRS).difference(present_attrs)
+            if missing:
+                logger.error(f'Mandatory attribute {missing} not presented')
+                request.saml2_edugainattrs = True
+                return None
 
             if isinstance(first_name, list):
                 first_name = first_name[0]
