a3aadbac346579eea8ed463d8ac36e57039951e1
Daniel Vrcic <daniel.vrcic@gmail.com>
2025-08-13 14:40:19 +0200

    mailinglist_unsubscribe django mgmt command

---
+++ hrzoosignup/backend/management/commands/mailinglist_unsubscribe.py
@@ -0,0 +1,56 @@
+import asyncio
+import logging
+import os
+
+from backend.httpq.excep import HZSIHttpError
+from backend.tasks.mailinglist import ListUnsubscribe
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+
+logger = logging.getLogger("hrzoosignup.crons")
+
+
+class Command(BaseCommand):
+    help = "Unsubscribe eligible users from mailing list"
+
+    def __init__(self):
+        super().__init__()
+        self.user_model = get_user_model()
+
+    def add_arguments(self, parser):
+        super(Command, self).add_arguments(parser)
+        parser.add_argument(
+            "--yes",
+            action="store_true",
+            dest="confirm_yes",
+            help="Make changes",
+        )
+
+    def handle(self, *args, **options):
+        logger.info("Unsubscribing eligible users from mailing list...")
+
+        all_users = self.user_model.objects.all()
+
+        users_to_unsubscribe = list()
+        for user in all_users:
+            if user.status == False and user.mailinglist_subscribe == True:
+                users_to_unsubscribe.append(user)
+
+        if not options.get('confirm_yes', None):
+            self.stdout.write(self.style.WARNING('List of users that will be unsubscribed'))
+            for user in users_to_unsubscribe:
+                self.stdout.write(self.style.WARNING(f'{user.username}'))
+        else:
+            try:
+                list_subscribe = ListUnsubscribe(users_to_unsubscribe)
+                asyncio.run(list_subscribe.run())
+                if users_to_unsubscribe:
+                    logger.info(f'User to unsubscribe: {repr([user.username for user in users_to_unsubscribe])}')
+                    logger.info(f'Details in {os.environ["VIRTUAL_ENV"]}/var/log/tasks.log ')
+                else:
+                    logger.info('No users to unsubscribe')
+
+            except (HZSIHttpError, KeyboardInterrupt):
+                pass
+
+            logger.info("Unsubscribing eligible users to mailing list... DONE")
