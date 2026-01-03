58ee9b59af037113df9308a69c8eb7f4503f0497
Daniel Vrcic <daniel.vrcic@gmail.com>
2026-01-03 11:11:31 +0100

    copyright year bumped

--- hrzoosignup/frontend/src/components/Footer.jsx
+++ hrzoosignup/frontend/src/components/Footer.jsx
@@ -33,7 +33,7 @@ const Footer = () => {
       </div>
       <div className="text-center pt-1 pb-2">
         <p>
-          <small>Copyright © 2025{' '}
+          <small>Copyright © 2026{' '}
             <a href={intl.formatMessage({
                 defaultMessage: "https://www.srce.unizg.hr/",
                 description: 'footer-link'
