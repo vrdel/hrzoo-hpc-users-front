b4d6a8bce9345e5ca620e06c24d5e1420b7a170c
Daniel Vrcic <daniel.vrcic@gmail.com>
2026-01-09 11:55:59 +0100

    define sizes of field for other widths on new request index

--- hrzoosignup/frontend/src/pages/new-requests/index.jsx
+++ hrzoosignup/frontend/src/pages/new-requests/index.jsx
@@ -110,7 +110,7 @@ const NewRequestIndex = () => {
       </Row>
       <Row className="mt-4">
         <Row>
-          <Col md={{size: 4, offset: 1}}>
+          <Col md={{size: 4, offset: 1}} sm={{size: 10}} lg={{size: 10, offset: 1}}  xl={{size: 10, offset: 1}} xxl={{size: 10, offset: 1}}>
             <Label
               htmlFor="requestUsesAI"
               aria-label="requestUsesAI"
@@ -124,7 +124,7 @@ const NewRequestIndex = () => {
           </Col>
         </Row>
         <Row>
-          <Col md={{size: 1, offset: 1}}>
+          <Col md={{size: 1, offset: 1}} lg={{offset: 1, size: 2}} xs={{size: 6}} sm={{size: 6}}>
             <Input
               aria-label="requestUsesAI"
               disabled={true}
