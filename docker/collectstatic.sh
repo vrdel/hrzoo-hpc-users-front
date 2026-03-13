37ff7ab7ac3aa5511c0d5043d71d49b147f1c2ef
Daniel Vrcic <daniel.vrcic@gmail.com>
2026-03-13 14:02:32 +0100

    py39 -> py312 replaced in apache, docker and helper scripts

--- docker/collectstatic.sh
+++ docker/collectstatic.sh
@@ -2,5 +2,5 @@
 
 . /opt/hrzoo-signup/bin/activate
 rm -rf $VIRTUAL_ENV/share/hrzoosignup/static/reactbundle/* ; \
-python $VIRTUAL_ENV/lib/python3.9/site-packages/hrzoosignup/manage.py collectstatic --noinput ; \
+python $VIRTUAL_ENV/lib/python3.12/site-packages/hrzoosignup/manage.py collectstatic --noinput ; \
 rm -rf $VIRTUAL_ENV/share/hrzoosignup/static/admin
