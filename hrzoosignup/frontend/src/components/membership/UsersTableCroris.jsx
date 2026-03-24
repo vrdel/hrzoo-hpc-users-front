af25f32a0e699748551e9069b246be4a456c5c45
Daniel Vrcic <daniel.vrcic@gmail.com>
2026-03-24 09:53:37 +0100

    remove sorting on UsersTableCroris for now

--- hrzoosignup/frontend/src/components/membership/UsersTableCroris.jsx
+++ hrzoosignup/frontend/src/components/membership/UsersTableCroris.jsx
@@ -19,7 +19,6 @@ import { toast } from 'react-toastify';
 import { FormattedMessage } from 'react-intl';
 import { useIntl } from 'react-intl'
 import { useOpenedIndexMap } from 'Hooks/indexed-map'
-import { SortArrow } from 'Components/TableHelpers';
 import _ from 'lodash';
 
 
@@ -39,11 +38,6 @@ export const UsersTableCroris = ({project, invites, onSubmit}) => {
   const [searchFirstName, setSearchFirstName] = useState('')
   const [searchLastName, setSearchLastName] = useState('')
   const [searchEmail, setSearchEmail] = useState('')
-
-  const [sortFirstName, setSortFirstName] = useState(undefined)
-  const [sortLastName, setSortLastName] = useState(undefined)
-  const [sortEmail, setSortEmail] = useState(undefined)
-
 
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => {
@@ -187,14 +181,6 @@ export const UsersTableCroris = ({project, invites, onSubmit}) => {
     allMembers.push(lead)
   allMembers = allMembers.concat(filteredJoined)
 
-  if (sortFirstName !== undefined)
-    allMembers = _.orderBy(allMembers, [u => u['user'].first_name?.toLowerCase() ?? ''], [sortFirstName ? 'asc' : 'desc'])
-  if (sortLastName !== undefined)
-    allMembers = _.orderBy(allMembers, [u => u['user'].last_name?.toLowerCase() ?? ''], [sortLastName ? 'asc' : 'desc'])
-  if (sortEmail !== undefined)
-    allMembers = _.orderBy(allMembers, [u => u['user'].person_mail?.toLowerCase() ?? ''], [sortEmail ? 'asc' : 'desc'])
-
-
   if (emailInvites !== undefined) {
     let email_invites = emailInvites.map(i => i.email)
 
@@ -218,14 +204,6 @@ export const UsersTableCroris = ({project, invites, onSubmit}) => {
     let filteredCollaborators = notJoinedCollaborators.filter(u =>
       filterUser(u.first_name, u.last_name, collabRole, u.email)
     )
-
-    if (sortFirstName !== undefined)
-      filteredCollaborators = _.orderBy(filteredCollaborators, [u => u.first_name?.toLowerCase() ?? ''], [sortFirstName ? 'asc' : 'desc'])
-    if (sortLastName !== undefined)
-      filteredCollaborators = _.orderBy(filteredCollaborators, [u => u.last_name?.toLowerCase() ?? ''], [sortLastName ? 'asc' : 'desc'])
-    if (sortEmail !== undefined)
-      filteredCollaborators = _.orderBy(filteredCollaborators, [u => u.email?.toLowerCase() ?? ''], [sortEmail ? 'asc' : 'desc'])
-
 
     let collabNoEmail = true
     for (var collab of missingCollab)
@@ -255,27 +233,17 @@ export const UsersTableCroris = ({project, invites, onSubmit}) => {
                   <th className="fw-normal" style={{width: '52px'}}>
                     #
                   </th>
-                  <th className="fw-normal" style={{cursor: 'pointer'}} onClick={() => { setSortFirstName(!sortFirstName); setSortLastName(undefined); setSortEmail(undefined) }}>
-                    <span className="d-flex">
-                      <span className="flex-grow-1">
-                        <FormattedMessage
-                          defaultMessage="Ime"
-                          description="users-table-croris-firstname"
-                        />
-                      </span>
-                      { SortArrow(sortFirstName !== undefined ? !sortFirstName : undefined) }
-                    </span>
+                  <th className="fw-normal">
+                    <FormattedMessage
+                      defaultMessage="Ime"
+                      description="users-table-croris-firstname"
+                    />
                   </th>
-                  <th className="fw-normal" style={{cursor: 'pointer'}} onClick={() => { setSortLastName(!sortLastName); setSortFirstName(undefined); setSortEmail(undefined) }}>
-                    <span className="d-flex">
-                      <span className="flex-grow-1">
-                        <FormattedMessage
-                          defaultMessage="Prezime"
-                          description="users-table-croris-lastname"
-                        />
-                      </span>
-                      { SortArrow(sortLastName !== undefined ? !sortLastName : undefined) }
-                    </span>
+                  <th className="fw-normal">
+                    <FormattedMessage
+                      defaultMessage="Prezime"
+                      description="users-table-croris-lastname"
+                    />
                   </th>
                   <th className="fw-normal">
                     <FormattedMessage
@@ -283,16 +251,11 @@ export const UsersTableCroris = ({project, invites, onSubmit}) => {
                       description="users-table-croris-role"
                     />
                   </th>
-                  <th className="fw-normal" style={{cursor: 'pointer'}} onClick={() => { setSortEmail(!sortEmail); setSortFirstName(undefined); setSortLastName(undefined) }}>
-                    <span className="d-flex">
-                      <span className="flex-grow-1">
-                        <FormattedMessage
-                          defaultMessage="Email"
-                          description="users-table-croris-email"
-                        />
-                      </span>
-                      { SortArrow(sortEmail !== undefined ? !sortEmail : undefined) }
-                    </span>
+                  <th className="fw-normal">
+                    <FormattedMessage
+                      defaultMessage="Email"
+                      description="users-table-croris-email"
+                    />
                   </th>
                   <th className="fw-normal" style={{minWidth: '180px'}}>
                     <FormattedMessage
